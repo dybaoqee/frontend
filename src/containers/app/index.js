903be746a387085d1b63bdfa7a99795170935a60
Gustavo Saiani <gustavo@poe.ma>
2017-07-25 15:09:20 +0200

    Add Listings path

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
+import Listings from '../listings'
 
 const App = () => (
   <div>
     <header>
       <Link to="/">Home</Link>
       <Link to="/about-us">About</Link>
+      <Link to="/listings">Listings</Link>
     </header>
 
     <main>
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
+      <Route exact path="/listings" component={Listings} />
     </main>
   </div>
 )
