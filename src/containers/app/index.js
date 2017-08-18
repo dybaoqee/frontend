492fa0bf39afe3d54d7ac857e29b2f2577cdbb47
Gustavo Saiani <gustavo@poe.ma>
2017-08-18 16:38:11 +0200

    Move listings to home page

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom'
-import Home from '../home'
-import About from '../about'
 import Listings from '../listings'
 
 const App = () => (
   <div>
     <header>
       <Link to="/">Home</Link>
-      <Link to="/about-us">About</Link>
-      <Link to="/listings">Listings</Link>
     </header>
 
     <main>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about-us" component={About} />
-      <Route exact path="/listings" component={Listings} />
+      <Route exact path="/" component={Listings} />
     </main>
   </div>
 )
