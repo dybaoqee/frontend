c7a62af7d97b121e8d121385aa215989e9830f08
Gustavo Saiani <gustavo@poe.ma>
2017-09-01 18:38:42 +0200

    All entries in index be links

--- src/containers/listings/index.js
+++ src/containers/listings/index.js
@@ -30,50 +30,53 @@ class Listings extends Component {
             }
 
             return <div className="listing" key={i}>
-              <div className="image-container" style={divStyle}>
-                <div>
-                  <span>R${listing.price}</span>
-                  <span className="address">
-                    {listing.address.street}
-                  </span>
-                  <span className="neighborhood">
-                    {listing.address.neighborhood}, {listing.address.city}
-                  </span>
-                </div>
-              </div>
-
-              <div className="text-container">
-                <div className="description">
-                  {listing.description}
+              <Link to={`listings/${listing.id}`}>
+                <div className="image-container" style={divStyle}>
+                  <div>
+                    <span>R${listing.price}</span>
+                    <span className="address">
+                      {listing.address.street}
+                    </span>
+                    <span className="neighborhood">
+                      {listing.address.neighborhood}, {listing.address.city}
+                    </span>
+                  </div>
                 </div>
 
-                <table>
-                  <tbody>
-                    <tr>
-                      <td>Quartos</td>
-                      <td>{listing.rooms}</td>
-                      <td>Vagas Garagem</td>
-                      <td>{listing.garage_spots}</td>
-                    </tr>
-                    <tr>
-                      <td>Banheiros</td>
-                      <td>{listing.bathrooms}</td>
-                      <td>Andar</td>
-                      <td>{listing.floor}</td>
-                    </tr>
-                    <tr>
-                      <td>Área</td>
-                      <td>{listing.area}</td>
-                      <td>R$/m²</td>
-                      <td>xxxx</td>
-                    </tr>
-                  </tbody>
-                </table>
+                <div className="text-container">
+                  <div className="description">
+                    {listing.description}
+                  </div>
 
-                <Link to={`/listings/${listing.id}`} className="btn">
-                  Ver Detalhes
-                </Link>
-              </div>
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>Quartos</td>
+                        <td>{listing.rooms}</td>
+                        <td>Vagas Garagem</td>
+                        <td>{listing.garage_spots}</td>
+                      </tr>
+                      <tr>
+                        <td>Banheiros</td>
+                        <td>{listing.bathrooms}</td>
+                        <td>Andar</td>
+                        <td>{listing.floor}</td>
+                      </tr>
+                      <tr>
+                        <td>Área</td>
+                        <td>{listing.area}</td>
+                        <td>R$/m²</td>
+                        <td>xxxx</td>
+                      </tr>
+                    </tbody>
+                  </table>
+
+                  <Link to={`listings/${listing.id}`} className="btn">
+
+                    Ver Detalhes
+                  </Link>
+                </div>
+              </Link>
             </div>
           })}
         </div>
