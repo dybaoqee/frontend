6a04caca0c0d79afb41a3ca20122e961b31b3cff
Gustavo Saiani <gustavo@poe.ma>
2017-10-09 21:33:25 -0300

    Extract listings index entries to component

--- src/containers/listings/index.js
+++ src/containers/listings/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import {
   fetchPostsIfNeeded
 } from '../../modules/listings/index'
+
+import Listing from "../../components/listings/index/listing"
 
 class Listings extends Component {
   componentDidMount() {
@@ -18,7 +19,7 @@ class Listings extends Component {
       backgroundImage: 'url(/mapa-leblon.jpg)'
     }
 
-    return <div>
+    return <div className="listings">
       <h1>Teste aqui pessoal</h1>
       <h2>Subtítulo do teste aqui pessoal</h2>
 
@@ -26,67 +27,14 @@ class Listings extends Component {
         <div>Fetching listings</div>}
 
       {index &&
-        <div className="listings">
-          <div style={varImg}>
+        <div>
+          <div className="map-container" style={varImg}>
             &nbsp;
           </div>
 
-          <div>
+          <div className="entries-container">
             {index.map((listing, i) => {
-              const bgImgUrl = `listings/photos/${listing.photo}`
-              const divStyle = {
-                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.0) 50%, rgba(0, 0, 0, 0.7) 80%), url(${bgImgUrl})`
-              }
-
-              return <div className="listing" key={i}>
-                <Link to={`listings/${listing.id}`}>
-                  <div className="image-container" style={divStyle}>
-                    <div>
-                      <span>R${listing.price}</span>
-                      <span className="address">
-                        {listing.address.street}
-                      </span>
-                      <span className="neighborhood">
-                        {listing.address.neighborhood}, {listing.address.city}
-                      </span>
-                    </div>
-                  </div>
-
-                  <div className="text-container">
-                    <div className="description">
-                      {listing.description}
-                    </div>
-
-                    <table>
-                      <tbody>
-                        <tr>
-                          <td>Quartos</td>
-                          <td>{listing.rooms}</td>
-                          <td>Vagas Garagem</td>
-                          <td>{listing.garage_spots}</td>
-                        </tr>
-                        <tr>
-                          <td>Banheiros</td>
-                          <td>{listing.bathrooms}</td>
-                          <td>Andar</td>
-                          <td>{listing.floor}</td>
-                        </tr>
-                        <tr>
-                          <td>Área</td>
-                          <td>{listing.area}</td>
-                          <td>R$/m²</td>
-                          <td>{Math.floor(listing.price / listing.area)}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-
-                    <Link to={`listings/${listing.id}`} className="btn">
-
-                      Ver Detalhes
-                    </Link>
-                  </div>
-                </Link>
-              </div>
+              return <Listing listing={listing} i={i} />
             })}
           </div>
         </div>
