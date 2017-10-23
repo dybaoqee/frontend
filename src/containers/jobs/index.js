c577c1a7981b2442fee59e127d523d4cfa9cfdf9
Gustavo Saiani <gustavo@poe.ma>
2017-10-23 21:38:15 -0200

    Add links to Google Forms in jobs

--- src/containers/jobs/index.js
+++ src/containers/jobs/index.js
@@ -7,7 +7,7 @@ class Jobs extends Component {
 
       <p>Criamos a EmCasa para melhorar a vida de quem está comprando e vendendo imóveis residenciais. Proporcionar mais qualidade, quantidade e velocidade de informação.</p>
 
-      <p>Já teve aqueles 20 minutos à toa naquele bairro que você sempre gostou e até sonhou morar? Imagine poder abrir um aplicativo e descobrir que num raio de 30 metros de você existem apartamentos à venda. Com um clique, abrir a porta do apartamento e poder vê-lo por dentro, sem depender de ninguém, sem esperar.</p>
+      <p>Já teve aqueles 20 minutos à toa naquele bairro que você sempre gostou e até sonhou morar? Imagine poder abrir um aplicativo e descobrir que num raio de 30 metros de você existem apartamentos à venda. Com um swipe, abrir a porta do apartamento e poder vê-lo por dentro, sem depender de ninguém, sem esperar.</p>
 
       <p>Nossa visão é proporcionar esse nível de experiência aos clientes EmCasa. Usar tecnologia para que todo o processo de compra, da procura à entrega da chave, seja muito superior ao que existe hoje.</p>
 
@@ -15,7 +15,7 @@ class Jobs extends Component {
 
       <br/>
 
-      <h5>A Equipe</h5>
+      <h5>A EQUIPE</h5>
 
       <p>Trabalharemos com uma equipe de desenvolvimento distribuída. Todas as habilidades necessárias para colaborar remotamente vão ser indispensáveis para nós também: disciplina, independência, iniciativa, comunicação.</p>
 
@@ -28,12 +28,26 @@ class Jobs extends Component {
 
       <p>O backend vai servir clientes web e mobile. Nosso objetivo é oferecer uma busca ágil e geolocalização. Experiência com ambas é um diferencial. Saber escrever código bem documentado e com testes é fundamental.</p>
 
+      <a href="https://docs.google.com/forms/d/1erD-GRBj629pKcFHpmwYOLz4OYbEHy8Mu5VB_o37EiM/" target="_blank" className="btn">
+        Cadastre-se
+      </a>
+
+      <br/>
+      <br/>
       <br/>
 
       <h2>Engenheiro JS / React / React Native</h2>
       <h4>Remoto</h4>
 
       <p>Desenvolvedor React ou React Native. Se for bom em ambos é um grande diferencial. Familiaridade com APIs do Google Maps é outro plus. Saber escrever código bem documentado e com testes é fundamental.</p>
+
+      <a href="https://docs.google.com/forms/d/1FIoJIqPfMLbh9qVj8ODJUNK08_yX_DbfhrRtF9oOWwA" target="_blank" className="btn">
+        Cadastre-se
+      </a>
+
+      <br/>
+      <br/>
+      <br/>
     </div>
   }
 }
