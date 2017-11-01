ec7a0e9d9a68b48494b89609917028b2353ff11d
Gustavo Saiani <gustavo@poe.ma>
2017-11-01 16:21:00 -0200

    Install react-helmet

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Helmet} from "react-helmet"
 import { Route } from 'react-router-dom'
 import Header from '../../components/header'
 import Listings from '../listings'
@@ -7,6 +8,13 @@ import Jobs from '../jobs'
 
 const App = () => (
   <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <meta name="description" content="Compre seu imóvel na Zona Sul do Rio de Janeiro." />
+      <meta name="og:image" content="www.example.com/teste" />
+      <title>EmCasa</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+    </Helmet>
 
     <Header />
 
