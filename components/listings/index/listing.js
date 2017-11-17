9c50598e060b281a0f7dcc326179e58593bef383
Gustavo Saiani <gustavo@poe.ma>
2017-11-17 18:01:54 -0200

    Virtual tour tag in home

--- components/listings/index/listing.js
+++ components/listings/index/listing.js
@@ -13,13 +13,33 @@ class Listing extends React.Component {
         <ImageContainer listing={listing} />
         <TextContainer listing={listing} />
 
+        {listing.matterport_code &&
+          <span className="matterport">Tour Virtual</span>
+        }
+
         <style jsx>{`
           .listings-entry {
             background: white;
             margin: 0 0 8px;
             overflow: auto;
+            position: relative;
             width: 100%;
           }
+
+          span.matterport {
+            background: rgba(240, 50, 50, 1);
+            color: white;
+            font-size: 9px;
+            font-stretch: condensed;
+            font-weight: bold;
+            padding: 4px 30px;
+            position: absolute;
+            right: -35px;
+            top: 24px;
+            text-transform: uppercase;
+            transform: rotate(45deg);
+          }
+
 
           @media (max-width: 600px) {
             .listings-entry > a {
