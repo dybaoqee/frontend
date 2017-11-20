972502c8fc9e5d0f3d4043b30eba9fc7d11b179d
Gustavo Saiani <gustavo@poe.ma>
2017-11-20 14:41:03 -0200

    Import media style call from outside file

---
+++ constants/media.js
@@ -0,0 +1,6 @@
+const minDeviceWidth = '320px'
+const maxDeviceWidth = '812px'
+
+export const mobileMedia = `
+ only screen and (min-device-width: ${minDeviceWidth}) and (max-device-width: ${maxDeviceWidth})
+`
