3aec3f3618a2578b8ac41ed74448bd6809c74d59
Gustavo Saiani <gustavo@poe.ma>
2017-11-24 18:26:39 -0200

    Functions to navigate images forward and backward

--- components/listings/listing/image-gallery.js
+++ components/listings/listing/image-gallery.js
@@ -3,12 +3,35 @@ import React from 'react'
 import { mobileMedia } from '../../../constants/media'
 
 export default class ImageGallery extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      imageIndex: 0
+    }
+  }
+
+  showNextImage = () => {
+    const { imageIndex } = this.state
+    this.setState({ imageIndex: imageIndex + 1 })
+  }
+
+  showPreviousImage = () => {
+    const { imageIndex } = this.state
+    this.setState({ imageIndex: imageIndex - 1 })
+  }
+
   render() {
     const { images } = this.props
+    const imagesLength = images.length
+    const { imageIndex } = this.state
+
+    const indexToShow = Math.abs((imagesLength - imageIndex) % imagesLength)
+
+    const imgSrc = process.env.REACT_APP_S3_BASE_URL + 'listings/original/' + images[indexToShow].filename
 
     return (
       <div>
-        <img src={process.env.REACT_APP_S3_BASE_URL + 'listings/original/' + images[0].filename} alt="Listing Main Pic"/>
+        <img src={imgSrc} onClick={this.showPreviousImage} alt="Listing Main Pic"/>
         <style jsx>{`
           img {
             max-width: 100vw;
