2e646387fa693b7e9e5330a91b001b70e80fc314
Gustavo Saiani <gustavo@poe.ma>
2017-12-06 18:06:05 -0200

    Pass all parameters to api in listings/new

--- services/listing-api.js
+++ services/listing-api.js
@@ -4,10 +4,25 @@ export const createListing = async (data, jwt) => {
 
   const payload = {
     listing: {
-      area: data.area
+      'description': data.description,
+      'price': data.price,
+      'floor': data.floor,
+      'rooms': data.rooms,
+      'bathrooms': data.bathrooms,
+      'area': data.area,
+      'garage_spots': data.garageSpots,
+      'score': data.score,
+      'matterport_code': data.matterportCode
     },
     address: {
-      street: data.street
+      'street': data.street,
+      'street_number': data.streetNumber,
+      'neighborhood': data.neighborhood,
+      'city': data.city,
+      'state': data.state,
+      'postal_code': data.postalCode,
+      'lat': data.lat,
+      'lng': data.lng
     }
   }
 
