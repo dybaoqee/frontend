30453503d327d1f809bce830624acf57c2aa51c5
Gustavo Saiani <gustavo@poe.ma>
2017-12-11 20:49:37 -0200

    Add staging api for env-config

--- env-config.js
+++ env-config.js
@@ -1,7 +1,17 @@
 const prod = process.env.NODE_ENV === 'production'
 
+let api_url
+
+if (!prod) {
+  api_url = 'http://localhost:4000/'
+} else if (process.env.IS_STAGING === 'true') {
+  api_url = 'https://em-casa-backend-staging.herokuapp.com'
+} else {
+  api_url = 'https://em-casa.herokuapp.com'
+}
+
 module.exports = {
   'process.env.NODE_ENV': prod ? 'production' : 'development',
-  'process.env.REACT_APP_API_URL': prod ? 'https://em-casa.herokuapp.com/' : 'http://localhost:4000/',
+  'process.env.REACT_APP_API_URL': api_url,
   'process.env.REACT_APP_S3_BASE_URL': 'https://s3-sa-east-1.amazonaws.com/emcasa/'
 }
