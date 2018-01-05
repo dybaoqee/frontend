6c52eb11b20cfb9151c2b6b14e08a77ad3929927
Gustavo Saiani <gustavo@poe.ma>
2018-01-05 18:25:13 -0200

    Add path to treat all filtering criteria

--- pages/filter.js
+++ pages/filter.js
@@ -4,6 +4,13 @@ import Router from 'next/router'
 import { getNeighborhoods } from '../services/neighborhood-api'
 
 export default class Filter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      bairros: {}
+    }
+  }
+
   static async getInitialProps(context) {
     const res = await getNeighborhoods()
 
@@ -16,27 +23,62 @@ export default class Filter extends Component {
     }
 
     return {
-      neighborhoods: res.data.neighborhoods
+      bairros: res.data.neighborhoods
     }
-
   }
 
   handleNeighborhoodChange = (e) => {
-    const neighborhood = e.target.value
-    Router.push(`/?bairro=${neighborhood}`).then(() => window.scrollTo(0, 0))
+    const { bairros } = this.state
+
+    if (bairros[e.target.value] == true) {
+      delete bairros[e.target.value]
+    } else {
+      bairros[e.target.value] = true
+    }
+
+    this.setState({ bairros: bairros })
+  }
+
+  joinParam = (param) => {
+    if (param !== null && typeof param === 'object') {
+      return Object.keys(param).map(function(key) {
+        return key
+      }).join('|')
+    } else {
+      return param
+    }
+  }
+
+  treatParams = () => {
+    const that = this
+
+    return Object.keys(this.state).map(function(key) {
+      const flattenedValue = that.joinParam(that.state[key])
+      if (flattenedValue === '') return ''
+      return `${key}=${flattenedValue}`
+    }).join('&')
+  }
+
+  handleSubmit = () => {
+    const { bairros } = this.state
+    const params = this.treatParams()
+    Router.push(`/?${params}`).then(() => window.scrollTo(0, 0))
   }
 
   render() {
-    const { neighborhoods } = this.props
+    const { bairros } = this.props
 
     return <div>
       <h1>Bairros</h1>
 
-      <select onChange={this.handleNeighborhoodChange}>
-      {neighborhoods && neighborhoods.map((neighborhood, i) => {
-        return <option key={i} label={neighborhood} value={neighborhood}/>
+      {bairros && bairros.map((bairro, i) => {
+        return <div key={i}>
+          <input type="checkbox" value={bairro} onClick={this.handleNeighborhoodChange} />
+          <label>{bairro}</label>
+        </div>
       })}
-      </select>
+
+      <button onClick={this.handleSubmit}>Go</button>
     </div>
   }
 }
