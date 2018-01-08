a91875969998e0d1fdc062e27589f6b6ec9a20bc
Gustavo Saiani <gustavo@poe.ma>
2018-01-08 17:35:06 -0200

    Toggle extra filter visibility

--- components/listings/index/filter.js
+++ components/listings/index/filter.js
@@ -1,10 +1,13 @@
 import { Component } from 'react'
 import Router from 'next/router'
 
+import * as colors from '../../../constants/colors'
+
 export default class Filter extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      areFiltersVisible: false,
       bairros: {}
     }
   }
@@ -55,52 +58,105 @@ export default class Filter extends Component {
     this.setState(state)
   }
 
+  handleToggleFilterVisibility = () => {
+    const state = this.state
+    state.areFiltersVisible = !state.areFiltersVisible
+    this.setState(state)
+  }
 
   render() {
     const { neighborhoods } = this.props
+    const { areFiltersVisible } = this.state
 
-    return <div>
-      {neighborhoods && neighborhoods.map((bairro, i) => {
-        return <div key={i}>
-          <input type="checkbox" value={bairro} onClick={this.handleNeighborhoodChange} />
-          <label>{bairro}</label>
+    console.log(areFiltersVisible);
+
+    return <div className="container">
+      <div className="price-container">
+        <div>
+          <label>Preço</label>
+          <input type="text" name="preco_minimo" onChange={this.handleInputChange} />
+          <label>a</label>
+          <input type="text" name="preco_maximo" onChange={this.handleInputChange} />
         </div>
-      })}
 
-      <div>
-        <label>Preço Mínimo</label>
-        <input type="text" name="preco_minimo" onChange={this.handleInputChange} />
+        {!!areFiltersVisible && <span onClick={this.handleToggleFilterVisibility}>
+          Menos Filtros<span>›</span>
+        </span>}
+
+        {!areFiltersVisible && <span onClick={this.handleToggleFilterVisibility}>
+          Mais Filtros<span>‹</span>
+        </span>}
       </div>
 
-      <div>
-        <label>Preço Máximo</label>
-        <input type="text" name="preco_maximo" onChange={this.handleInputChange} />
-      </div>
 
-      <div>
-        <label>Área Mínima</label>
-        <input type="text" name="area_minima" onChange={this.handleInputChange} />
-      </div>
+      {!!areFiltersVisible && <div>
+        {neighborhoods && neighborhoods.map((bairro, i) => {
+          return <div key={i}>
+            <input type="checkbox" value={bairro} onClick={this.handleNeighborhoodChange} />
+            <label>{bairro}</label>
+          </div>
+        })}
 
-      <div>
-        <label>Área Máxima</label>
-        <input type="text" name="area_maxima" onChange={this.handleInputChange} />
-      </div>
 
-      <div>
-        <label>Quartos</label>
-        <input type="text" name="quartos" onChange={this.handleInputChange} />
-      </div>
+        <div>
+          <label>Área Mínima</label>
+          <input type="text" name="area_minima" onChange={this.handleInputChange} />
+        </div>
+
+        <div>
+          <label>Área Máxima</label>
+          <input type="text" name="area_maxima" onChange={this.handleInputChange} />
+        </div>
+
+        <div>
+          <label>Quartos</label>
+          <input type="text" name="quartos" onChange={this.handleInputChange} />
+        </div>
+
+        <button onClick={this.handleSubmit}>Go</button>
+      </div>}
+
       <style jsx>{`
-        div,
+        div.container {
+          border-bottom: 1px solid ${colors.lightGray};
+          > div {
+            display: block;
+            &.price-container {
+              align-items: center;
+              display: flex;
+              justify-content: space-between;
+              span {
+                  color: ${colors.blue};
+                  cursor: pointer;
+                  float: right;
+                  margin-right: 20px;
+                  > span {
+                    margin-left: 5px;
+                    transform: rotate(-90deg);
+                  }
+                }
+              }
+            div {
+              padding: 10px;
+            }
+          }
+        }
+
+        label {
+          margin-right: 10px;
+        }
+
+        input {
+          margin-right: 10px;
+          padding: 10px;
+        }
+
         button {
           clear: both;
           float: left;
           margin: 10px 0;
         }
       `}</style>
-
-      <button onClick={this.handleSubmit}>Go</button>
     </div>
   }
 }
