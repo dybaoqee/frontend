500d312ce6ce1196a03321d7af817df0213fb13e
Gustavo Saiani <gustavo@poe.ma>
2018-01-22 18:33:25 -0200

    Add controls to close filter params in listings/index

--- components/listings/index/filter.js
+++ components/listings/index/filter.js
@@ -112,10 +112,10 @@ export default class Filter extends Component {
   }
 
   setAllParamFiltersVisibilityToFalse = () => {
-    const { filterVisibility } = this.state
+    const { state } = this
+    const { filterVisibility } = state
 
-    const state = this.state
-    return Object.keys(filterVisibility).map(function(key) {
+    Object.keys(filterVisibility).map(function(key) {
       state.filterVisibility[key] = false
     })
 
@@ -137,108 +137,153 @@ export default class Filter extends Component {
 
     const { areFiltersVisible, filterVisibility } = this.state
 
-    return <div className="container">
-      {this.isAnyParamFilterOpen() && <div className="active-filter-overlay" />}
+    return <div className={"container "+ (this.isAnyParamFilterOpen() ? 'filter-open' : '')}>
+      {
+        this.isAnyParamFilterOpen() &&
+        <div className="active-filter-overlay" onClick={this.setAllParamFiltersVisibilityToFalse} />
+      }
+
 
       <span className="filter-title">Filtros</span>
 
       <div className="filter-param-container">
-        <button onClick={this.togglePriceFilterVisibility}>Preço</button>
+        <button
+          className={filterVisibility.price ? 'active' : ''}
+          onClick={this.togglePriceFilterVisibility}
+        >
+          Preço
+        </button>
 
         {filterVisibility.price &&
           <div className="option-container price-container">
-            <label>De</label>
+            <div>
+              <label>De</label>
 
-            <Select
-              name="form-field-name"
-              arrowRenderer={null}
-              style={{width: 130}}
-              placeholder="R$"
-              value={preco_minimo}
-              onChange={this.handleMinPriceChange}
-              options={filterOptions.minPriceOptions}
-              searchable={false} />
+              <Select
+                name="form-field-name"
+                arrowRenderer={null}
+                style={{width: 130}}
+                placeholder="R$"
+                value={preco_minimo}
+                onChange={this.handleMinPriceChange}
+                options={filterOptions.minPriceOptions}
+                searchable={false} />
 
-            <label>a</label>
+              <label>a</label>
 
-            <Select
-              name="form-field-name"
-              arrowRenderer={null}
-              style={{width: 130}}
-              placeholder="R$"
-              value={preco_maximo}
-              onChange={this.handleMaxPriceChange}
-              options={filterOptions.maxPriceOptions}
-              searchable={false} />
+              <Select
+                name="form-field-name"
+                arrowRenderer={null}
+                style={{width: 130}}
+                placeholder="R$"
+                value={preco_maximo}
+                onChange={this.handleMaxPriceChange}
+                options={filterOptions.maxPriceOptions}
+                searchable={false} />
+            </div>
+
+            <span className="close-filter-param" onClick={this.setAllParamFiltersVisibilityToFalse}>
+              Aplicar
+            </span>
           </div>
         }
       </div>
 
       <div className="filter-param-container">
-        <button onClick={this.toggleAreaFilterVisibility}>Área</button>
+        <button
+          className={filterVisibility.area ? 'active' : ''}
+          onClick={this.toggleAreaFilterVisibility}
+        >
+          Área
+        </button>
 
         {filterVisibility.area &&
           <div className="option-container">
-            <label>De</label>
+            <div>
+              <label>De</label>
 
-            <Select
-              name="form-field-name"
-              arrowRenderer={null}
-              style={{width: 100}}
-              placeholder="m²"
-              value={area_minima}
-              onChange={this.handleMinAreaChange}
-              options={filterOptions.minAreaOptions}
-              searchable={false} />
+              <Select
+                name="form-field-name"
+                arrowRenderer={null}
+                style={{width: 100}}
+                placeholder="m²"
+                value={area_minima}
+                onChange={this.handleMinAreaChange}
+                options={filterOptions.minAreaOptions}
+                searchable={false} />
 
-            <label>a</label>
+              <label>a</label>
 
-            <Select
-              name="form-field-name"
-              arrowRenderer={null}
-              style={{width: 100}}
-              placeholder="m²"
-              value={area_maxima}
-              onChange={this.handleMaxAreaChange}
-              options={filterOptions.maxAreaOptions}
-              searchable={false} />
+              <Select
+                name="form-field-name"
+                arrowRenderer={null}
+                style={{width: 100}}
+                placeholder="m²"
+                value={area_maxima}
+                onChange={this.handleMaxAreaChange}
+                options={filterOptions.maxAreaOptions}
+                searchable={false} />
+            </div>
+            <span className="close-filter-param" onClick={this.setAllParamFiltersVisibilityToFalse}>
+              Aplicar
+            </span>
           </div>
         }
       </div>
 
       <div className="filter-param-container">
-        <button onClick={this.toggleRoomFilterVisibility}>Quartos</button>
+        <button
+          className={filterVisibility.rooms ? 'active' : ''}
+          onClick={this.toggleRoomFilterVisibility}
+        >
+          Quartos
+        </button>
 
         {filterVisibility.rooms &&
           <div className="option-container">
-            <Select
-              name="form-field-name"
-              arrowRenderer={null}
-              style={{width: 130}}
-              placeholder="Nº Quartos"
-              value={quartos}
-              onChange={this.handleRoomChange}
-              options={filterOptions.roomNumberOptions}
-              searchable={false} />
+            <div>
+              <Select
+                name="form-field-name"
+                arrowRenderer={null}
+                style={{width: 130}}
+                placeholder="Nº Quartos"
+                value={quartos}
+                onChange={this.handleRoomChange}
+                options={filterOptions.roomNumberOptions}
+                searchable={false} />
+            </div>
+            <span className="close-filter-param" onClick={this.setAllParamFiltersVisibilityToFalse}>
+              Aplicar
+            </span>
           </div>
         }
       </div>
 
       <div className="filter-param-container">
-        <button onClick={this.toggleNeighborhoodsFilterVisibility}>Bairros</button>
+        <button
+          className={filterVisibility.neighborhoods ? 'active' : ''}
+          onClick={this.toggleNeighborhoodsFilterVisibility}
+        >
+          Bairros
+        </button>
 
         {filterVisibility.neighborhoods &&
           <div className="option-container">
-            <Select
-              name="form-field-name"
-              arrowRenderer={null}
-              style={{width: 200}}
-              placeholder="Bairros"
-              multi={true}
-              value={bairros}
-              onChange={this.handleNeighborhoodChange}
-              options={neighborhoodOptions}
-              searchable={false} />
+            <div>
+              <Select
+                name="form-field-name"
+                arrowRenderer={null}
+                style={{width: 200}}
+                placeholder="Bairros"
+                multi={true}
+                value={bairros}
+                onChange={this.handleNeighborhoodChange}
+                options={neighborhoodOptions}
+                searchable={false} />
+            </div>
+            <span className="close-filter-param" onClick={this.setAllParamFiltersVisibilityToFalse}>
+              Aplicar
+            </span>
           </div>
         }
       </div>
@@ -277,7 +322,7 @@ export default class Filter extends Component {
       <style jsx>{`
 
         div.active-filter-overlay {
-          background: rgba(255, 255, 255, 0.85);
+          background: rgba(100, 100, 100, 0.85);
           height: calc(100vh - 155px);
           position: absolute;
           top: 78px;
@@ -296,6 +341,9 @@ export default class Filter extends Component {
           position: fixed;
           width: 100vw;
           z-index: 4;
+          &.filter-open {
+            box-shadow: 1px 1px 4px ${colors.lightGray};
+          }
         }
 
         div.filter-param-container {
@@ -303,26 +351,44 @@ export default class Filter extends Component {
         }
 
         div.option-container {
-          align-items: center;
           background: white;
           border: 1px solid ${colors.lightGray};
-          border-bottom-left-radius: 6px;
-          border-bottom-right-radius: 6px;
           border-top: 1px solid white;
-          display: flex;
+          box-shadow: 0px 2px 2px ${colors.lightestGray};
+          height: calc(100vh - 190px);
+          justify-content: space-between;
           margin-right: 40px;
           padding: 20px;
           position: absolute;
           top: 67px;
+
+          div {
+            align-items: center;
+            display: flex;
+          }
+
           label {
             color: ${colors.blue};
             font-size: 12px;
             font-weight: 700;
             text-transform: uppercase;
+            &:last-of-type {
+              margin: 0 10px;
+            }
           }
 
-          label:last-of-type {
-            margin: 0 10px;
+          span.close-filter-param {
+            color: ${colors.blue};
+            display: block;
+            cursor: pointer;
+            float: right;
+            font-size: 12px;
+            font-weight: 700;
+            margin-top: 15px;
+            text-transform: uppercase;
+            &:hover {
+              color: ${colors.darkenedBlue};
+            }
           }
         }
 
@@ -391,6 +457,14 @@ export default class Filter extends Component {
           padding: 7px 20px 10px;
           &:hover {
             background: ${colors.offWhite}
+          }
+          &.active {
+            background: ${colors.blue};
+            color: white;
+            border: 1px solid ${colors.darkenedBlue};
+            &:hover {
+              background: ${colors.darkenedBlue};
+            }
           }
         }
 
