2c2d414baf45dc23c85dd7ac80ad4e1de351f816
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2018-01-25 13:51:27 -0200

    Remove staging from sslRedirect function parameters.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const handle = app.getRequestHandler()
 app.prepare()
 .then(() => {
   const server = express()
-  server.use(sslRedirect(['production', 'staging']))
+  server.use(sslRedirect(['production']))
 
   server.get('/jobs', (req, res) => {
     return app.render(req, res, '/jobs', req.query)
