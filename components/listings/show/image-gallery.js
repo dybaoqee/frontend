ee5d6aba8e5096bfdb3604e9954ee6ae0373e6c5
Gustavo Saiani <gustavo@poe.ma>
2018-02-02 17:16:38 -0200

    Navigate images in gallery

--- components/listings/show/image-gallery.js
+++ components/listings/show/image-gallery.js
@@ -14,22 +14,28 @@ export default class ImageGallery extends React.Component {
     const imgSrc = imageUrl(imgFilename)
 
     return (
-
       <div className="container">
+        {images.map(function(image, index) {
+          const imgSrc = imageUrl(image.filename)
+          const display = (index == indexToShow) ? 'block' : 'none';
+          const style = {
+            backgroundImage: `url(${imgSrc})`,
+            display: display,
+          }
+
+          return <div key={index} className="image" style={style}/>
+        })}
+
         <button onClick={handleHide}>
           ×
         </button>
 
-        {(imagesLength > 1) && <div onClick={handlePrevious}>‹</div>}
-        {(imagesLength > 1) && <div onClick={handleNext}>›</div>}
+        {(imagesLength > 1) && <div className="nav" onClick={handlePrevious}>‹</div>}
+        {(imagesLength > 1) && <div className="nav" onClick={handleNext}>›</div>}
 
         <style jsx>{`
           div.container {
             background-color: rgba(0, 0, 0, 0.85);
-            background-image: url(${imgSrc});
-            background-position: center;
-            background-repeat: no-repeat;
-            background-size: contain;
             height: 100vh;
             left: 0;
             max-width: 100vw;
@@ -40,7 +46,7 @@ export default class ImageGallery extends React.Component {
             z-index: 9;
           }
 
-          div > div {
+          div.nav {
             align-items: center;
             display: flex;
             color: white;
@@ -51,20 +57,30 @@ export default class ImageGallery extends React.Component {
             padding: 0 30px;
             user-select: none;
             width: calc(50% - 60px);
+            z-index: 10;
+            &:hover {
+              background: linear-gradient(to right, rgba(0, 0, 0, .333), rgba(0, 0, 0, 0));
+            }
+            &:last-of-type {
+              float: right;
+              justify-content: flex-end;
+              text-align: right;
+              &:hover {
+                background: linear-gradient(to left, rgba(0, 0, 0, .333), rgba(0, 0, 0, 0));
+              }
+            }
           }
 
-          div > div:hover {
-            background: linear-gradient(to right, rgba(0, 0, 0, .333), rgba(0, 0, 0, 0));
-          }
-
-          div > div:last-of-type {
-            float: right;
-            justify-content: flex-end;
-            text-align: right;
-          }
-
-          div > div:last-of-type:hover {
-            background: linear-gradient(to left, rgba(0, 0, 0, .333), rgba(0, 0, 0, 0));
+          div.image {
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: contain;
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 100vh;
+            width: 100vw;
+            z-index: -1;
           }
 
           button {
@@ -78,6 +94,7 @@ export default class ImageGallery extends React.Component {
             position: absolute;
             right: 10px;
             top: 10px;
+            z-index: 3;
             &:hover {
               background: rgba(255, 255, 255, 0.1);
             }
