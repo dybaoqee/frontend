4bfca3b47b3bd546672f78cceacff2bb0ebb77b6
Gustavo Saiani <gustavo@poe.ma>
2018-02-05 17:32:00 -0200

    Make map render in listings/show mobile

--- components/listings/show/map.js
+++ components/listings/show/map.js
@@ -11,17 +11,19 @@ export default class ListingMap extends React.Component {
 
     return <div>
       <MediaQuery query="(max-width: 600px)">
-        <MapContainer lat={listing.address.lat}
+        <MapContainer
+          lat={listing.address.lat}
           lng={listing.address.lng}
           width='100vw'
-          height='300px'/>
+          height='300px' />
       </MediaQuery>
 
       <MediaQuery query="(min-width: 601px)">
-        <MapContainer lat={listing.address.lat}
+        <MapContainer
+          lat={listing.address.lat}
           lng={listing.address.lng}
           width='960px'
-          height='500px'/>
+          height='500px' />
       </MediaQuery>
 
       <style jsx>{`
