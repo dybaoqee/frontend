9cfdbf22ffcea7386dfb5634a7c0d25725904611
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-02-15 17:20:52 -0200

    Split Listing component

--- components/home/listings.js
+++ components/home/listings.js
@@ -1,10 +1,8 @@
 import {Component} from 'react'
-import Link from 'next/link'
-import NumberFormat from 'react-number-format'
 
 import * as colors from 'constants/colors'
 import {mobileMedia} from 'constants/media'
-import {mainListingThumbnail} from 'utils/image_url'
+import Listing from "../shared/Listing"
 
 export default class HomeListings extends Component {
   render() {
@@ -13,34 +11,7 @@ export default class HomeListings extends Component {
 
     return (
       <div className="container">
-        {listings.slice(0, numberOfListingsToShow).map(function(listing) {
-          const imgUrl = mainListingThumbnail(listing.images)
-          const imgStyle = {backgroundImage: `url(${imgUrl})`}
-
-          return (
-            <Link
-              href={`/listings/show?id=${listing.id}`}
-              as={`/imoveis/${listing.id}`}
-              key={listing.id}
-              prefetch
-            >
-              <div className="listing">
-                <div className="image-container" style={imgStyle} />
-                <p className="price">
-                  <NumberFormat
-                    value={listing.price}
-                    displayType={'text'}
-                    thousandSeparator={'.'}
-                    prefix={'R$'}
-                    decimalSeparator={','}
-                  />
-                </p>
-                <p className="street">{listing.address.street}</p>
-                <p className="neighborhood">{listing.address.neighborhood}</p>
-              </div>
-            </Link>
-          )
-        })}
+        {listings.slice(0, numberOfListingsToShow).map(listing => <Listing {...listing} />)}
 
         <style jsx>{`
           div.container {
@@ -49,49 +20,6 @@ export default class HomeListings extends Component {
             margin: 0 auto;
             padding: 60px 0;
             width: 1180px;
-          }
-
-          div.listing {
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-            cursor: pointer;
-            float: left;
-            font-size: 20px;
-            height: 300px;
-            overflow: hidden;
-            width: 282px;
-            &:hover {
-              box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
-            }
-
-            p {
-              clear: both;
-              float: left;
-              margin: 0 20px;
-              &.price {
-                font-size: 24px;
-                font-weight: 300;
-                margin-top: 18px;
-              }
-              &.street {
-                color: ${colors.mediumDarkGray};
-                font-size: 14px;
-                margin-bottom: 4px;
-                margin-top: 2px;
-              }
-              &.neighborhood {
-                color: ${colors.mediumDarkGray};
-                font-size: 14px;
-                text-transform: uppercase;
-              }
-            }
-          }
-
-          div.image-container {
-            background-position: center;
-            background-size: cover;
-            height: 180px;
-            width: 100%;
           }
 
           @media ${mobileMedia} {
---
+++ components/shared/Listing/styles.js
@@ -0,0 +1,45 @@
+import * as colors from 'constants/colors'
+import {mobileMedia} from 'constants/media'
+import styled from "styled-components"
+
+export default styled.div`
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  float: left;
+  font-size: 20px;
+  height: 300px;
+  overflow: hidden;
+  width: 282px;
+  &:hover {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+  }
+
+  > p {
+    clear: both;
+    float: left;
+    margin: 0 20px;
+    &.price {
+      font-size: 24px;
+      font-weight: 300;
+      margin-top: 18px;
+    }
+    &.street {
+      color: ${colors.mediumDarkGray};
+      font-size: 14px;
+      margin-bottom: 4px;
+      margin-top: 2px;
+    }
+    &.neighborhood {
+      color: ${colors.mediumDarkGray};
+      font-size: 14px;
+      text-transform: uppercase;
+    }
+  }
+  > .image-container {
+    background-position: center;
+    background-size: cover;
+    height: 180px;
+    width: 100%;
+  }
+`
