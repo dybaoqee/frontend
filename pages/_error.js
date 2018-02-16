8d728f9647d24cc3420be93c5925298a0d3bc813
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-02-16 19:43:41 -0200

    Add error page

---
+++ pages/_error.js
@@ -0,0 +1,47 @@
+import {Component} from 'react'
+
+import {isAuthenticated} from 'lib/auth'
+import Layout from 'components/main-layout'
+
+export default class ErrorPage extends Component {
+
+  static getInitialProps({err, res, ...ctx}) {
+    if (err && err.response) {
+      res.statusCode = err.response.status
+    }
+    return {
+      err,
+      statusCode: res.statusCode,
+      authenticated: isAuthenticated(ctx)
+    }
+  }
+
+  get title() {
+    const {err, statusCode} = this.props
+
+    switch (statusCode) {
+      case 404: return 'Página não encontrada'
+      case 500: return 'Internal Server Error'
+      default: return err ? err.message : ''
+    }
+  }
+
+  get message() {
+    const {statusCode} = this.props
+
+    switch (statusCode) {
+      case 404: return 'O link que você clicou pode estar quebrado ou a página pode ter sido removida.'
+      default: return null
+    }
+  }
+
+  render() {
+    const {authenticated} = this.props
+    return (
+      <Layout authenticated={authenticated}>
+        <h1>{this.title}</h1>
+        <p>{this.message}</p>
+      </Layout>
+    )
+  }
+}
