6fab260588304cb5e53da8c7ae56adcee7465e6e
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-02-20 16:12:06 -0300

    Fix marker size

--- components/listings/index/Map/Marker.js
+++ components/listings/index/Map/Marker.js
@@ -1,15 +1,19 @@
 import {Marker} from 'react-gmaps'
 
-const ICON_URL = 'http://maps.google.com/mapfiles/ms/micons/'
+const ICON_URL = 'https://maps.gstatic.com/mapfiles/api-3/images'
 
-const getSize = (num) => ({width: num, height: num})
+const getSize = (scale) => ({
+  width: 27 * scale,
+  height: 43 * scale
+})
 
 export default function ListingsMarker({relevance, size, ...props}) {
+  const scale = relevance + Math.abs((relevance - 1) * 0.5)
   return (
     <Marker
       icon={{
-        scaledSize: getSize(Math.ceil(relevance * size)),
-        url: `${ICON_URL}/red.png`
+        scaledSize: getSize(scale),
+        url: `${ICON_URL}/spotlight-poi2.png`
       }}
       {...props}
     />
