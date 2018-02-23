f0bd18926f373a9ac1c7d9b487fd55f17d65193b
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-02-23 18:34:27 -0300

    Fix sniffr initialization

--- utils/polyfills/user-agent.js
+++ utils/polyfills/user-agent.js
@@ -2,6 +2,6 @@ import Sniffr from 'sniffr'
 
 const s = new Sniffr()
 
-export default (typeof window !== 'undefined'
-  ? s.sniff(window.navigator.userAgent)
-  : undefined)
+if (typeof window !== 'undefined') s.sniff(window.navigator.userAgent)
+
+export default s
