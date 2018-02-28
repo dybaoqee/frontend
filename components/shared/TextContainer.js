ae7ed92df69bf2a1676eb58f31a907adce054196
Gustavo Saiani <gustavo@poe.ma>
2018-02-27 23:24:31 -0300

    Set min-height for TextContainer

--- components/shared/TextContainer.js
+++ components/shared/TextContainer.js
@@ -1,9 +1,12 @@
 import {Component} from 'react'
 
 import {mobileMedia} from 'constants/media'
+import {desktopHeaderHeight} from 'constants/dimensions'
 
 export default class TextContainer extends Component {
   render() {
+    const minHeightPadding = desktopHeaderHeight + 220
+
     return (
       <div>
         {this.props.children}
@@ -11,6 +14,7 @@ export default class TextContainer extends Component {
         <style jsx>{`
           div {
             margin: 0 auto 100px;
+            min-height: calc(100vh - ${minHeightPadding}px);
             padding-top: 40px;
             width: 700px;
           }
--- constants/dimensions.js
+++ constants/dimensions.js
@@ -1,5 +1,5 @@
 export const desktopHeaderHeight = 76
 export const desktopFilterHeight = 58
 
-export const desktopHeaderAndFilterHeight = desktopHeaderHeight + desktopFilterHeight
-
+export const desktopHeaderAndFilterHeight =
+  desktopHeaderHeight + desktopFilterHeight
