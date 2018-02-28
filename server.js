ed0d1e0b8b2f13545c1e0506c8329b302c837723
Gustavo Saiani <gustavo@poe.ma>
2018-02-27 23:14:34 -0300

    Basic 'Sell Your Property' page

--- server.js
+++ server.js
@@ -4,94 +4,99 @@ const sslRedirect = require('heroku-ssl-redirect')
 
 const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000
-const app = next({ dir: '.', dev })
+const app = next({dir: '.', dev})
 const handle = app.getRequestHandler()
 
-app.prepare()
-.then(() => {
-  const server = express()
-  server.use(sslRedirect(['production'], 301))
+app
+  .prepare()
+  .then(() => {
+    const server = express()
+    server.use(sslRedirect(['production'], 301))
 
-  if(process.env.NODE_ENV === 'production') {
-    server.use((req, res, next) => {
-      if (req.hostname === 'localhost' || req.subdomains.length > 0) return next()
-      res.redirect(301, `https://www.${req.headers.host}${req.url}`)
+    if (process.env.NODE_ENV === 'production') {
+      server.use((req, res, next) => {
+        if (req.hostname === 'localhost' || req.subdomains.length > 0)
+          return next()
+        res.redirect(301, `https://www.${req.headers.host}${req.url}`)
+      })
+    }
+
+    server.get('/jobs', (req, res) => {
+      return app.render(req, res, '/jobs', req.query)
     })
-  }
 
-  server.get('/jobs', (req, res) => {
-    return app.render(req, res, '/jobs', req.query)
+    server.get('/venda-seu-imovel', (req, res) => {
+      return app.render(req, res, '/sell', req.query)
+    })
+
+    server.get('/indique', (req, res) => {
+      return app.render(req, res, '/indique', req.query)
+    })
+
+    server.get('/logout', (req, res) => {
+      return app.render(req, res, '/auth/logout', req.query)
+    })
+
+    server.get('/login', (req, res) => {
+      return app.render(req, res, '/auth/login', req.query)
+    })
+
+    server.get('/signup', (req, res) => {
+      return app.render(req, res, '/auth/signup', req.query)
+    })
+
+    server.get('/confirmar_cadastro/:token', (req, res) => {
+      app.render(req, res, '/auth/confirm', req.query)
+    })
+
+    server.get('/imoveis', (req, res) => {
+      const actualPage = '/listings/index'
+      app.render(req, res, actualPage, req.query)
+    })
+
+    server.get('/imoveis/adicionar', (req, res) => {
+      const actualPage = '/listings/new'
+      app.render(req, res, actualPage)
+    })
+
+    server.get('/imoveis/:id', (req, res) => {
+      const actualPage = '/listings/show'
+      const queryParams = {id: req.params.id}
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/imoveis/:id/editar', (req, res) => {
+      const actualPage = '/listings/edit'
+      const queryParams = {id: req.params.id}
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/imoveis/:listingId/imagens', (req, res) => {
+      const actualPage = '/listings/images'
+      const queryParams = {listingId: req.params.listingId}
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/user/:id', (req, res) => {
+      const actualPage = '/user'
+      const queryParams = {id: req.params.id}
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/busca', (req, res) => {
+      return app.render(req, res, '/search', req.query)
+    })
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(port, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
   })
-
-  server.get('/indique', (req, res) => {
-    return app.render(req, res, '/indique', req.query)
+  .catch((ex) => {
+    console.error(ex.stack)
+    process.exit(1)
   })
-
-  server.get('/logout', (req, res) => {
-    return app.render(req, res, '/auth/logout', req.query)
-  })
-
-  server.get('/login', (req, res) => {
-    return app.render(req, res, '/auth/login', req.query)
-  })
-
-  server.get('/signup', (req, res) => {
-    return app.render(req, res, '/auth/signup', req.query)
-  })
-
-  server.get('/confirmar_cadastro/:token', (req, res) => {
-    app.render(req, res, '/auth/confirm', req.query)
-  })
-
-  server.get('/imoveis', (req, res) => {
-    const actualPage = '/listings/index'
-    app.render(req, res, actualPage, req.query)
-  })
-
-  server.get('/imoveis/adicionar', (req, res) => {
-    const actualPage = '/listings/new'
-    app.render(req, res, actualPage)
-  })
-
-  server.get('/imoveis/:id', (req, res) => {
-    const actualPage = '/listings/show'
-    const queryParams = { id: req.params.id }
-    app.render(req, res, actualPage, queryParams)
-  })
-
-  server.get('/imoveis/:id/editar', (req, res) => {
-    const actualPage = '/listings/edit'
-    const queryParams = { id: req.params.id }
-    app.render(req, res, actualPage, queryParams)
-  })
-
-  server.get('/imoveis/:listingId/imagens', (req, res) => {
-    const actualPage = '/listings/images'
-    const queryParams = { listingId: req.params.listingId }
-    app.render(req, res, actualPage, queryParams)
-  })
-
-  server.get('/user/:id', (req, res) => {
-    const actualPage = '/user'
-    const queryParams = { id: req.params.id }
-    app.render(req, res, actualPage, queryParams)
-  })
-
-  server.get('/busca', (req, res) => {
-    return app.render(req, res, '/search', req.query)
-  })
-
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
-
-  server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
-
