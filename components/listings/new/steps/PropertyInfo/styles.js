8b1015f5f76ff79e7d07665361991925c72e09cd
Nathan Queija <nathanqcam@gmail.com>
2018-03-01 19:25:58 -0300

    Fixing styles logic on listings/new

--- components/listings/new/steps/PropertyInfo/styles.js
+++ components/listings/new/steps/PropertyInfo/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import * as colors from 'constants/colors'
-import {notMobileMedia, mobileMedia} from 'constants/media'
+import {mobileMedia} from 'constants/media'
 import {Field} from '../../shared/styles'
 
 const FieldContainer = styled.div`
@@ -9,22 +9,24 @@ const FieldContainer = styled.div`
   grid-template-columns: repeat(6, minmax(50px, 1fr));
   grid-gap: 20px;
 
-  @media ${notMobileMedia} {
-    > ${Field}:nth-child(1) {
-      grid-column: span 2;
-    }
+  > ${Field}:nth-child(1) {
+    grid-column: span 2;
+  }
 
-    > ${Field}:nth-child(2) {
-      grid-column: span 3;
-    }
+  > ${Field}:nth-child(2) {
+    grid-column: span 3;
+  }
 
-    > ${Field}:nth-child(n + 4):not(:nth-child(n + 10)) {
-      grid-column: span 2;
-    }
+  > ${Field}:nth-child(n + 4):not(:nth-child(n + 10)) {
+    grid-column: span 2;
   }
 
   @media ${mobileMedia} {
-    grid-template-columns: 1fr;
+    display: block;
+
+    > ${Field} {
+      margin-bottom: 10px;
+    }
   }
 `
 
