94bbeba7ea60e44152a5b72b73cfd28d98109e6b
Gustavo Saiani <gustavo@poe.ma>
2018-03-02 12:03:50 -0300

    Install google-map-react

--- components/listings/index/Map/index.js
+++ components/listings/index/Map/index.js
@@ -1,16 +1,19 @@
-import {Marker} from 'react-gmaps'
+import {InfoWindow} from 'react-gmaps'
 
 import Map from 'components/shared/Map'
+import Container from './styles'
 
 export default function ListingsMap({children, onSelect, ...props}) {
   return (
     <Map {...props}>
       {Array.from(children).map(([pageNum, listings]) =>
         listings.map((listing) => (
-          <Marker
+          <InfoWindow
             key={listing.id}
             lat={listing.address.lat}
             lng={listing.address.lng}
+            content={'yo'}
+            onCloseClick={false}
             onClick={() => onSelect(listing.id)}
           />
         ))
