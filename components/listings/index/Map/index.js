792f0cb11b2c9b0d0b451aea74b0cbc8d3d164b2
Gustavo Saiani <gustavo@poe.ma>
2018-03-05 13:23:35 -0300

    Extract lat, lng correctly from listings

--- components/listings/index/Map/index.js
+++ components/listings/index/Map/index.js
@@ -2,7 +2,8 @@ import Map from 'components/shared/Map'
 
 export default function ListingsMap({listings, onSelect, ...props}) {
   const markers = listings.map((listing) => {
-    const {lat, lng, price} = listing
+    const {price} = listing
+    const {lat, lng} = listing.address
     return {lat: lat, lng: lng, text: price}
   })
 
