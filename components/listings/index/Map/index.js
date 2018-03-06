a697ee14e4c24135fd407ac9c6f20e0d320fb0a4
Gustavo Saiani <gustavo@poe.ma>
2018-03-05 21:10:06 -0300

    Use commas instead of dots in listing/index Map

--- components/listings/index/Map/index.js
+++ components/listings/index/Map/index.js
@@ -5,7 +5,7 @@ export default function ListingsMap({listings, ...props}) {
   const markers = listings.map((listing) => {
     const {id, price} = listing
     const {lat, lng} = listing.address
-    const abbreviatedPrice = abbreviate(price, 2).toUpperCase()
+    const abbreviatedPrice = abbreviate(price, 2).toUpperCase().replace('.', ',')
     return {id, lat, lng, text: abbreviatedPrice}
   })
 
