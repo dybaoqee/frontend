23ffbeecaaf49924a945458934c178900c399485
Gustavo Saiani <gustavo@poe.ma>
2018-03-08 12:16:32 -0300

    Use styled components for image upload component

---
+++ components/listings/show/images/Upload/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  clear: both;
+  div {
+    align-items: center;
+    display: flex;
+    height: 80px !important;
+    margin-bottom: 20px;
+    justify-content: center;
+    width: calc(100% - 4px) !important;
+  }
+`
