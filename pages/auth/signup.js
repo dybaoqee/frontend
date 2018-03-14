fed951bfc13eccc96ab65390ef93c8c80d4962ef
Nathan Queija <nathanqcam@gmail.com>
2018-03-14 15:30:41 -0300

    Add loading condition on login & signup

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,15 +1,17 @@
 import {Component} from 'react'
-import Link from 'next/link'
 
 import Layout from 'components/shared/Shell'
-import Form from 'components/auth/Form'
-import Error from 'components/auth/Error'
+import Form from 'components/shared/Common/Form'
+import Errors from 'components/shared/Common/Errors'
+import EmCasaButton from 'components/shared/Common/Buttons'
 import {getCookie, removeCookie} from 'lib/session'
-import {signIn, signUp, redirectIfAuthenticated} from 'lib/auth'
+import {signUp, redirectIfAuthenticated} from 'lib/auth'
+import _ from 'lodash'
 
 export default class Login extends Component {
   state = {
-    error: null,
+    errors: [],
+    loading: false
   }
 
   static getInitialProps(ctx) {
@@ -22,54 +24,43 @@ export default class Login extends Component {
         removeCookie('success')
       }
       return {
-        success,
+        success
       }
     }
   }
 
   handleSubmit = async (e) => {
     e.preventDefault()
+    this.setState({errors: [], loading: true})
 
     const name = e.target.elements.name.value
     const email = e.target.elements.email.value
     const password = e.target.elements.password.value
 
-    const error = await signUp(name, email, password)
-
-    if (error) {
-      console.log(error)
-      this.setState({
-        error,
-      })
-      return false
+    let errors = await signUp(name, email, password)
+    if (errors) {
+      errors = errors && _.isArray(errors) ? errors : [errors]
+      this.setState({errors})
     }
+
+    this.setState({loading: false})
   }
 
   render() {
-    const {url, success} = this.props
-    const {error} = this.state
+    const {errors, loading} = this.state
 
     return (
       <Layout>
-        <div>
-          <Form handleSubmit={this.handleSubmit}>
-            <h1>Cadastre-se</h1>
-
-            <div className="control-group">
-              <input type="text" placeholder="Nome" name="name" />
-            </div>
-
-            <div className="control-group">
-              <input type="email" placeholder="Email" name="email" />
-            </div>
-
-            <div className="control-group">
-              <input type="password" placeholder="Senha" name="password" />
-            </div>
-
-            <button type="submit">Enviar</button>
-          </Form>
-        </div>
+        <Form onSubmit={this.handleSubmit}>
+          <h1>Cadastre-se</h1>
+          <input type="text" placeholder="Nome" name="name" />
+          <input type="email" placeholder="Email" name="email" />
+          <input type="password" placeholder="Senha" name="password" />
+          <EmCasaButton disabled={loading} full type="submit">
+            {loading ? 'Aguarde...' : 'Enviar'}
+          </EmCasaButton>
+          <Errors errors={errors} />
+        </Form>
       </Layout>
     )
   }
