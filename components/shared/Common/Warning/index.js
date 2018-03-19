df17eeca23fdbb0526ca31e7f8b49006b0e379af
Nathan Queija <nathanqcam@gmail.com>
2018-03-19 18:00:01 -0300

    Remove Warning Icon

--- components/shared/Common/Warning/index.js
+++ components/shared/Common/Warning/index.js
@@ -1,9 +1,6 @@
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import faWarning from '@fortawesome/fontawesome-free-solid/faExclamationTriangle'
 import Container from './styles'
 export default ({message}) => (
   <Container>
-    <FontAwesomeIcon icon={faWarning} />
     <span>{message || 'Aviso'}</span>
   </Container>
 )
