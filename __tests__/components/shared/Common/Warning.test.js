15bcb05831b2a2ac40b2a2f0a2072e9945fb919e
Gustavo Saiani <gustavo.saiani@emcasa.com>
2018-03-21 13:44:16 -0300

    Improve test title wordings

--- __tests__/components/shared/Common/Warning.test.js
+++ __tests__/components/shared/Common/Warning.test.js
@@ -5,27 +5,27 @@ import * as colors from 'constants/colors'
 import 'jest-styled-components'
 
 describe('<Warning />', () => {
-  it('should render with blue background if no color props is passed', () => {
+  it('should render with blue background if no color prop is passed', () => {
     const wrapper = shallow(<Warning />)
     expect(wrapper).toHaveStyleRule('background', colors.blue.light)
   })
 
-  it('should render with blue background if color props is undefined', () => {
+  it('should render with blue background if color prop is undefined', () => {
     const wrapper = shallow(<Warning green={undefined} />)
     expect(wrapper).toHaveStyleRule('background', colors.blue.light)
   })
 
-  it('should render with green background green props is passed', () => {
+  it('should render with green background if green prop is passed', () => {
     const wrapper = shallow(<Warning green />)
     expect(wrapper).toHaveStyleRule('background', colors.green.light)
   })
 
-  it('should render with orange background orange props is passed', () => {
+  it('should render with orange background if orange prop is passed', () => {
     const wrapper = shallow(<Warning orange />)
     expect(wrapper).toHaveStyleRule('background', colors.orange.light)
   })
 
-  it('should render with red background if red props is passed', () => {
+  it('should render with red background if red prop is passed', () => {
     const wrapper = shallow(<Warning red />)
     expect(wrapper).toHaveStyleRule('background', colors.red.light)
   })
