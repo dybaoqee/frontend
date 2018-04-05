d4f6c200d04ffcf0708859f14ae8b0f52bb1114b
Nathan Queija <nathanqcam@gmail.com>
2018-04-05 11:27:18 -0300

    Add styles to Main Container to enable sticky footer

---
+++ components/shared/Shell/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  margin-top: 76px;
+  display: flex;
+  box-sizing: border-box;
+  min-height: calc(100vh - 76px);
+  flex-direction: column;
+`
+
+export const Main = styled.main`
+  flex: 1;
+`
