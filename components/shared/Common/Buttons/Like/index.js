27cac5817788dfc2eeddb4b11a495d4ab5dc5358
Nathan Queija <nathanqcam@gmail.com>
2018-04-10 23:22:39 -0300

    If user is authenticated fav listing

--- components/shared/Common/Buttons/Like/index.js
+++ components/shared/Common/Buttons/Like/index.js
@@ -4,22 +4,24 @@ import faHeart from '@fortawesome/fontawesome-free-solid/faHeart'
 import {Mutation} from 'react-apollo'
 import {FAVORITE_LISTING, UNFAVORITE_LISTING} from 'graphql/listings/mutations'
 import {GET_FAVORITE_LISTINGS_IDS} from 'graphql/user/queries'
-export default (props) => (
+import Router from 'next/router'
+import {setCookie} from 'lib/session'
+const LikeButton = (props) => (
   <Mutation mutation={!props.favorite ? FAVORITE_LISTING : UNFAVORITE_LISTING}>
     {(favoriteListing) => (
       <Button
         {...props}
         onClick={() => {
-          favoriteListing({
-            refetchQueries: [
-              {
-                query: GET_FAVORITE_LISTINGS_IDS
+          if (props.user && props.user.authenticated) {
+            favoriteListing({
+              refetchQueries: [
+                {
+                  query: GET_FAVORITE_LISTINGS_IDS
+                }
+              ],
+              variables: {
+                id: props.listing.id
               }
-            ],
-            variables: {
-              id: props.listing.id
-            }
-          })
         }}
       >
         <FontAwesomeIcon icon={faHeart} />
