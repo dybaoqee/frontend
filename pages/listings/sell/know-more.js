e9499975bb036722bdf3e54d40b24f0999a070aa
Nathan Queija <nathanqcam@gmail.com>
2018-04-12 16:08:10 -0300

    Add page layout

--- pages/listings/sell/know-more.js
+++ pages/listings/sell/know-more.js
@@ -39,28 +39,117 @@ export default class SellKnowMore extends Component {
           <meta name="twitter:description" content={seoDescription} />
           <meta name="twitter:image" content={seoImg} />
         </Head>
-
-        <Topics title="Venda seu imóvel" showNumbers>
-          <p>
-            Anuncie seu imóvel por apenas 3% do valor da venda. Os clientes
-            EmCasa poupam, em média, R$20mil e a gente só recebe a comissão
-            quando você recebe o valor da venda.
-          </p>
-          <p>
-            O tour virtual permite fazer mais visitas, ocupando menos tempo e
-            com mais segurança. Venda seu imóvel mais rápido! - "Vendi meu
-            imóvel em 12 dias com a EmCasa" - Graciano
-          </p>
-          <p>
-            Tenha assistência jurídica no processo inteiro (documentação,
-            financiamento, FGTS) sem pagar mais por isso
-          </p>
-        </Topics>
-        <CallToAction
-          call="Vender imóvel"
-          href={'/listings/new'}
-          as={'/imoveis/adicionar'}
-        />
+        <Fragment>
+          <Header>
+            <img
+              src="https://res.cloudinary.com/emcasa/image/upload/f_auto/v1513818385/home-2018-04-03_cozxd9.jpg"
+              alt="Venda seu imóvel na EmCasa"
+            />
+            <h1>Venda seu imóvel na EmCasa</h1>
+          </Header>
+          <Container>
+            <h2>Benefícios da EmCasa</h2>
+            <BenefitsContainer>
+              <Benefit>
+                <div />
+                <p>Anuncie e Venda seu Imóvel com a EmCasa.</p>
+              </Benefit>
+              <Benefit>
+                <div />
+                <p>Tenha acesso imediato à milhares de compradores.</p>
+              </Benefit>
+              <Benefit>
+                <div />
+                <p>
+                  Ganhe um Tour Virtual em 3D e atraia mais atenção ao seu
+                  imóvel.
+                </p>
+              </Benefit>
+              <Benefit>
+                <div />
+                <p>
+                  Economize tempo e evite visitas desnecessárias em sua casa.
+                </p>
+              </Benefit>
+              <Benefit>
+                <div />
+                <p>Economize dinheiro com nossa comissão reduzida de 3%.</p>
+              </Benefit>
+              <Benefit>
+                <div />
+                <p>Receba um relatório de visitas e interesse do seu imóvel.</p>
+              </Benefit>
+              <Benefit>
+                <div />
+                <p>Suporte em Financiamento e retirada de FGTS.</p>
+              </Benefit>
+              <Benefit>
+                <div />
+                <p>Assistência jurídica grátis com documentação e processos.</p>
+              </Benefit>
+            </BenefitsContainer>
+            <Link href={'/listings/new'} as={'/imoveis/adicionar'}>
+              <EmCasaButton>Anuncie agora</EmCasaButton>
+            </Link>
+          </Container>
+          <Container>
+            <h2>Como funciona</h2>
+            <h3>
+              Faça um rápido cadastro e cadastre seu imóvel em menos de 5
+              minutos.
+            </h3>
+            <Topics showNumbers>
+              <div>
+                <h4>Faça seu cadastro online</h4>
+              </div>
+              <div>
+                <h4>Envie as fotos do imóvel</h4>
+              </div>
+              <div>
+                <h4>
+                  Envie os documentos<span>
+                    (Apenas para validação interna)
+                  </span>
+                </h4>
+              </div>
+            </Topics>
+            <p className="warning">
+              <strong>Pronto!</strong> Seu imóvel estará no ar e nossa equipe
+              iniciará o processo de anuncio e venda de seu apartamento ou casa.
+              Agora é aguardar nosso contato para agendamento das visitas com os
+              interessados em comprar o seu imóvel
+            </p>
+            <Link href={'/listings/new'} as={'/imoveis/adicionar'}>
+              <EmCasaButton>Anuncie agora</EmCasaButton>
+            </Link>
+          </Container>
+          <Container>
+            <h2>Aproveite também</h2>
+            <CardContainer>
+              <Card>
+                <div />
+                <div>
+                  <h5>Avaliação de imóvel gratuita</h5>
+                  <p>
+                    Responda um Questionário e faremos uma avaliação do valor de
+                    venda gratuitamente
+                  </p>
+                </div>
+              </Card>
+              <Card>
+                <div />
+                <div>
+                  <h5>Ganhe o Tour Virtual em 3D</h5>
+                  <p>
+                    Faça o agendamento online e receba nossa equipe uma visita
+                    de aprox. 45 min. Nesta visita tiraremos fotos profissionais
+                    e produziremos o Tour Virtual
+                  </p>
+                </div>
+              </Card>
+            </CardContainer>
+          </Container>
+        </Fragment>
       </Layout>
     )
   }
