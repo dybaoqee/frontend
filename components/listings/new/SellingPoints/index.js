224b932ca5d0d165789f5cb149b5f1f88db1bf77
Nathan Queija <nathanqcam@gmail.com>
2018-04-13 17:35:43 -0300

    Add know more link

--- components/listings/new/SellingPoints/index.js
+++ components/listings/new/SellingPoints/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Container from './styles'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faCheck from '@fortawesome/fontawesome-free-solid/faCheckCircle'
@@ -15,5 +16,8 @@ export default () => (
       <FontAwesomeIcon icon={faCheck} />
       <span>Assistência jurídica e financeira</span>
     </div>
+    <Link href="/listings/sell/know-more" as="/saiba-mais-para-vender">
+      <a title="Saiba mais para vender">Quer saber ainda mais?</a>
+    </Link>
   </Container>
 )
