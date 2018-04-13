3f7e99f99313109f824009fa08a809ce28b8a9fe
Gustavo Saiani <gustavo@poe.ma>
2018-04-13 20:23:13 -0300

    Style listings/new selling points

--- components/listings/new/SellingPoints/index.js
+++ components/listings/new/SellingPoints/index.js
@@ -4,6 +4,7 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faCheck from '@fortawesome/fontawesome-free-solid/faCheckCircle'
 export default () => (
   <Container>
+    <h6>Uma imobiliária nova de verdade</h6>
     <div>
       <FontAwesomeIcon icon={faCheck} />
       <span>Melhores condições de mercado</span>
