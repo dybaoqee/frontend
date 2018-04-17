fc4e9d9881c513fdcc963ee55f670586a233f5b1
Nathan Queija <nathanqcam@gmail.com>
2018-04-17 12:01:34 -0300

    Move styles to styled components

--- components/listings/index/Listing/index.js
+++ components/listings/index/Listing/index.js
@@ -69,58 +69,7 @@ class Listing extends React.Component {
         {listing.matterport_code && (
           <span className="matterport">Tour Virtual</span>
         )}
-
-        <style jsx>{`
-          div {
-            background: white;
-            border-bottom: 1px solid ${colors.lightGray};
-            cursor: pointer;
-            overflow: hidden;
-            padding-bottom: 20px;
-            padding-top: 20px;
-            position: relative;
-            width: 100%;
-          }
-
-          div:hover {
-            background: #f5f5f5;
-          }
-
-          div:first-of-type {
-            padding-top: 23px;
-          }
-
-          span.matterport {
-            background: rgba(240, 50, 50, 1);
-            color: white;
-            font-size: 9px;
-            font-stretch: condensed;
-            font-weight: bold;
-            padding: 4px 30px;
-            position: absolute;
-            right: -35px;
-            top: 24px;
-            text-transform: uppercase;
-            transform: rotate(45deg);
-          }
-
-          @media ${mobileMedia} {
-            .listing > a {
-              flex-direction: column;
-              text-decoration: none;
-            }
-          }
-        `}</style>
-
-        <style jsx global>{`
-          .listing:hover table {
-            border-bottom: 1px solid #ccc;
-          }
-          .listing:hover table tr td {
-            border-top: 1px solid #ccc !important;
-          }
-        `}</style>
-      </div>
+      </Container>
     )
   }
 }
