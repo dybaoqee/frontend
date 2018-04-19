69db4ea708ea9760f39f8e6cc0019f914fc3268f
Nathan Queija <nathanqcam@gmail.com>
2018-04-19 11:06:37 -0300

    Fix image margin and add new styled components

--- components/listings/index/Listing/styles.js
+++ components/listings/index/Listing/styles.js
@@ -32,11 +32,55 @@ export default styled.div`
   }
 `
 export const LikeContainer = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
   display: flex;
   justify-content: flex-end;
-  margin-top: 5px;
+  align-items: flex-start;
 
   @media ${mobileMedia} {
     justify-content: flex-start;
+  }
+`
+
+export const ImageContainer = styled.div`
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+  border-radius: 8px;
+  float: left;
+  height: 200px;
+  margin-left: 10px;
+  position: relative;
+  width: 290px;
+  margin-top: 4px;
+
+  @media ${mobileMedia} {
+    width: calc(100% - 20px);
+    margin-top: 0;
+  }
+`
+
+export const ListingInfo = styled.div`
+  bottom: 10px;
+  color: white;
+  left: 10px;
+  position: absolute;
+
+  span {
+    clear: both;
+    display: block;
+    font-size: 18px;
+  }
+
+  span.address {
+    font-size: 13px;
+    margin-bottom: 1px;
+    margin-top: 3px;
+  }
+
+  span.neighborhood {
+    font-size: 10px;
   }
 `
