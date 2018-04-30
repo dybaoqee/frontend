db21cc6d67c6a57edb3de76edf8f8b10a64d5588
Nathan Queija <nathanqcam@gmail.com>
2018-04-30 17:02:18 -0300

    Move to folder and turn into functional components when possible

---
+++ components/listings/index/Listing/Table/index.js
@@ -0,0 +1,43 @@
+import NumberFormat from 'react-number-format'
+import Container from './styles'
+
+export default ({
+  listing: {price, area, rooms, garage_spots, bathrooms, floor}
+}) => {
+  const price_per_square_meter = Math.floor(price / area)
+
+  return (
+    <Container>
+      <table cellSpacing="0">
+        <tbody>
+          <tr>
+            <td>Quartos</td>
+            <td>{rooms}</td>
+            <td>Vagas Garagem</td>
+            <td>{garage_spots}</td>
+          </tr>
+          <tr>
+            <td>Banheiros</td>
+            <td>{bathrooms}</td>
+            <td>Andar</td>
+            <td>{floor}</td>
+          </tr>
+          <tr>
+            <td>Área</td>
+            <td>{area}</td>
+            <td>Preço/m²</td>
+            <td>
+              <NumberFormat
+                value={price_per_square_meter}
+                displayType={'text'}
+                thousandSeparator={'.'}
+                prefix={'R$'}
+                decimalSeparator={','}
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
