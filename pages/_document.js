ee077d6ec94aba66023f19dd6fd7c9bea6340257
Nathan Queija <nathanqcam@gmail.com>
2018-05-03 09:42:20 -0300

    Remove tags and add Google Tag Manager

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,15 @@ export default class AppDocument extends Document {
     return (
       <html>
         <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5ZF5DFW');`
+            }}
+          />
           {styleTags}
           {globalStyles}
           <link
@@ -28,49 +37,6 @@ export default class AppDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800"
             rel="stylesheet"
-          />
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${
-              process.env.GOOGLE_ANALYTICS_TRACKING_ID
-            }`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || []
-                function gtag(){
-                  dataLayer.push(arguments)
-                }
-                gtag('js', new Date())
-                gtag('config', '${process.env.GOOGLE_ANALYTICS_TRACKING_ID}')
-
-                <!-- Facebook Pixel Code -->
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window,document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '710980862398359');
-                fbq('track', 'PageView');
-                <!-- End Facebook Pixel Code -->
-              `
-            }}
-          />
-
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `
-                <!-- Facebook Pixel Code -->
-                <img height="1" width="1"
-                src="https://www.facebook.com/tr?id=710980862398359&ev=PageView
-                &noscript=1"/>
-                <!-- End Facebook Pixel Code -->
-              `
-            }}
           />
 
           {/* Start code for Google AdWords */}
@@ -91,40 +57,6 @@ export default class AppDocument extends Document {
             src="//www.googleadservices.com/pagead/conversion.js"
           />
 
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `
-                <div style="display:inline;">
-                  <img
-                    height="1"
-                    width="1"
-                    style="border-style:none;"
-                    alt=""
-                    src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/830164609/?guid=ON&amp;script=0"
-                  />
-                </div>
-              `
-            }}
-          />
-          {/* End code for Google AdWords */}
-
-          {/* Start code for Hotjar */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:784178,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-              `
-            }}
-          />
-          {/* End code for Hotjar */}
-
           <meta
             name="viewport"
             content="initial-scale=1.0, width=device-width"
@@ -139,6 +71,14 @@ export default class AppDocument extends Document {
           />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5ZF5DFW"
+              height="0"
+              width="0"
+              style={{display: 'none', visibility: 'hidden'}}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
