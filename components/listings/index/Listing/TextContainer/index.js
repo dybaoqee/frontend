adb4b95c371edc52e1b2a6d6d16f6504f62bf0c7
Nathan Queija <nathanqcam@gmail.com>
2018-05-04 15:46:59 -0300

    Remove unused variable

--- components/listings/index/Listing/TextContainer/index.js
+++ components/listings/index/Listing/TextContainer/index.js
@@ -6,13 +6,13 @@ import Container from './styles'
 
 export default class TextContainer extends Component {
   truncateDescription = () => {
-    const {id, description} = this.props.listing
+    const {description} = this.props.listing
 
     if (!description) return
 
     var re = description.match(/^.{0,160}[\S]*/)
     var l = re[0].length
-    var re = re[0].replace(/\s$/, '')
+    re = re[0].replace(/\s$/, '')
     if (l < description.length) {
       re = re + '…'
     }
