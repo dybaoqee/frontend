f7987e3204a3c77083618919f1fcd8391f32b83e
Nathan Queija <nathanqcam@gmail.com>
2018-05-10 16:56:49 -0300

    Center span on aggregator container

--- components/shared/Map/ClusterMarker/styles.js
+++ components/shared/Map/ClusterMarker/styles.js
@@ -1,20 +1,27 @@
 import styled from 'styled-components'
 import * as colors from 'constants/colors'
 export default styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 13px;
-  height: 13px;
-  padding: 7px 8px 9px;
+  box-sizing: border-box;
+  width: 35px;
+  height: 35px;
   margin-left: -10px;
   text-align: center;
   font-size: 14px;
   color: #fff;
   border: 2px solid #fff;
-  border-radius: 50%;
+  border-radius: 100%;
   background-color: ${colors.blue.medium};
   box-shadow: 1px 3px 6px rgba(0, 0, 0, 0.3);
+
+  > span {
+    box-sizing: border-box;
+    padding: 2px 0 3px 0;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
   ${({highlight}) =>
     highlight
