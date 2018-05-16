a0dd36b50d1227c8cfb81be3504c641e159d5cd9
Nathan Queija <nathanqcam@gmail.com>
2018-05-15 23:43:02 -0300

    Add pages wrapper

---
+++ pages/_app.js
@@ -0,0 +1,39 @@
+import App, {Container} from 'next/app'
+import _ from 'lodash'
+import Layout from 'components/shared/Shell'
+import {isAuthenticated, isAdmin} from 'lib/auth'
+export default class MyApp extends App {
+  static async getInitialProps({Component, router, ctx}) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return {
+      pageProps,
+      url: {
+        query: router.query,
+        pathname: router.pathname,
+        asPath: router.asPath
+      },
+      authenticated: isAuthenticated(ctx),
+      isAdmin: isAdmin(ctx)
+    }
+  }
+
+  render() {
+    const {Component, pageProps, url, authenticated, isAdmin} = this.props
+    return (
+      <Container>
+        <Layout
+          authenticated={authenticated}
+          isAdmin={isAdmin}
+          renderFooter={_.isUndefined(pageProps.renderFooter) ? true : false}
+        >
+          <Component {...pageProps} url={url} />
+        </Layout>
+      </Container>
+    )
+  }
+}
