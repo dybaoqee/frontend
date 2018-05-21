46c23391c616479782db9c1db62d51d2b0c03e0e
Nathan Queija <nathanqcam@gmail.com>
2018-05-21 13:13:04 -0300

    Use subscription inside Layout component

--- components/shared/Shell/index.js
+++ components/shared/Shell/index.js
@@ -6,6 +6,8 @@ import NProgress from 'nprogress'
 import Header from './Header'
 import Footer from './Footer'
 import Container, {Main} from './styles'
+import {Subscription} from 'react-apollo'
+import {MESSAGE_SENT} from 'graphql/messenger/subscriptions'
 
 Router.onRouteChangeStart = () => {
   NProgress.start()
@@ -17,28 +19,38 @@ Router.onRouteChangeComplete = () => {
 Router.onRouteChangeError = () => NProgress.done()
 
 class Layout extends Component {
+  constructor(props) {
+    super(props)
+    this.notifications = 0
+  }
   render() {
     const {authenticated, isAdmin, errorCode, renderFooter} = this.props
 
     return (
-      <Fragment>
-        <Head>
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/styles/nprogress.css"
-          />
-        </Head>
-        <Header
-          errorCode={errorCode}
-          authenticated={authenticated}
-          isAdmin={isAdmin}
-        />
-        <Container>
-          <Main>{this.props.children}</Main>
-          {renderFooter && <Footer />}
-        </Container>
-      </Fragment>
+      <Subscription subscription={MESSAGE_SENT}>
+        {({data, loading, error}) => (
+          <Fragment>
+            {data && this.notifications++}
+            <Head>
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/styles/nprogress.css"
+              />
+            </Head>
+            <Header
+              errorCode={errorCode}
+              authenticated={authenticated}
+              isAdmin={isAdmin}
+              notifications={this.notifications}
+            />
+            <Container>
+              <Main>{this.props.children}</Main>
+              {renderFooter && <Footer />}
+            </Container>
+          </Fragment>
+        )}
+      </Subscription>
     )
   }
 }
