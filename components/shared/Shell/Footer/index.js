ca3c8dd7542a68ce2e95de4b5d8ae1e36a249555
Nathan Queija <nathanqcam@gmail.com>
2018-05-22 10:42:15 -0300

    Add PreFooter

--- components/shared/Shell/Footer/index.js
+++ components/shared/Shell/Footer/index.js
@@ -12,63 +12,63 @@ import Container, {EmCasaInfo, EmCasaContact} from './styles'
 import PreFooter from 'components/shared/Shell/Footer/PreFooter'
 
 const Footer = () => (
-  <Container>
-    <EmCasaInfo>
-      <Link href="/">
-        <a>
-          <img
-            src="/static/emcasa-imobiliaria-rio-de-janeiro.png"
-            alt="Emcasa Imobiliária no Rio de Janeiro"
-          />
-        </a>
-      </Link>
-    </EmCasaInfo>
+  <Fragment>
+    <PreFooter />
+    <Container>
+      <EmCasaInfo>
+        <Link href="/">
+          <a>
+            <img
+              src="/static/emcasa-imobiliaria-rio-de-janeiro.png"
+              alt="Emcasa Imobiliária no Rio de Janeiro"
+            />
+          </a>
+        </Link>
+      </EmCasaInfo>
 
-    <EmCasaContact>
-      <a href="mailto:contato@emcasa.com">contato@emcasa.com</a>
+      <EmCasaContact>
+        <span> CRECI-RJ J-7712</span>
+        <a href="mailto:contato@emcasa.com">contato@emcasa.com</a>
+        <Link href="/sitemap">
+          <a>Mapa do site</a>
+        </Link>
 
-      <Link href="/jobs">
-        <a>Trabalhe Conosco</a>
-      </Link>
-      <Link href="/sitemap">
-        <a>Mapa do site</a>
-      </Link>
+        <div className="icons">
+          <a
+            href="https://www.facebook.com/EmCasa"
+            target="_blank"
+            className="icon"
+          >
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
 
-      <div className="icons">
-        <a
-          href="https://www.facebook.com/EmCasa"
-          target="_blank"
-          className="icon"
-        >
-          <FontAwesomeIcon icon={faFacebook} />
-        </a>
+          <a
+            href="https://www.instagram.com/emcasaimoveis/"
+            target="_blank"
+            className="icon"
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
 
-        <a
-          href="https://www.instagram.com/emcasaimoveis/"
-          target="_blank"
-          className="icon"
-        >
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
+          <a
+            href="https://www.linkedin.com/company/emcasa/"
+            target="_blank"
+            className="icon"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
 
-        <a
-          href="https://www.linkedin.com/company/emcasa/"
-          target="_blank"
-          className="icon"
-        >
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
-
-        <a
-          href="https://twitter.com/EmCasaTech"
-          target="_blank"
-          className="icon"
-        >
-          <FontAwesomeIcon icon={faTwitter} />
-        </a>
-      </div>
-    </EmCasaContact>
-  </Container>
+          <a
+            href="https://twitter.com/EmCasaTech"
+            target="_blank"
+            className="icon"
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </div>
+      </EmCasaContact>
+    </Container>
+  </Fragment>
 )
 
 export default Footer
