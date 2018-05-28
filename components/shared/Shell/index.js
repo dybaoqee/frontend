9b272da27eabb19ca12708488fa8e6ee4a2c874b
Nathan Queija <nathanqcam@gmail.com>
2018-05-28 16:20:07 -0300

    Remove Subscription

--- components/shared/Shell/index.js
+++ components/shared/Shell/index.js
@@ -6,8 +6,6 @@ import NProgress from 'nprogress'
 import Header from './Header'
 import Footer from './Footer'
 import Container, {Main} from './styles'
-import {Subscription} from 'react-apollo'
-import {MESSAGE_SENT} from 'graphql/messenger/subscriptions'
 
 Router.onRouteChangeStart = () => {
   NProgress.start()
@@ -21,11 +19,6 @@ Router.onRouteChangeError = () => NProgress.done()
 class Layout extends Component {
   constructor(props) {
     super(props)
-    this.notifications = 0
-  }
-
-  updateNotifications() {
-    this.notifications++
   }
   render() {
     const {
@@ -39,31 +32,26 @@ class Layout extends Component {
     } = this.props
 
     return (
-      <Subscription subscription={MESSAGE_SENT}>
-        {({data, loading, error}) => (
-          <Fragment>
-            {data && this.updateNotifications()}
-            <Head>
-              <link
-                rel="stylesheet"
-                type="text/css"
-                href="/static/styles/nprogress.css"
-              />
-            </Head>
-            <Header
-              errorCode={errorCode}
-              authenticated={authenticated}
-              isAdmin={isAdmin}
-              notifications={this.notifications}
-              router={router}
-            />
-            <Container>
-              <Main>{this.props.children}</Main>
-              {renderFooter && <Footer />}
-            </Container>
-          </Fragment>
-        )}
-      </Subscription>
+      <Fragment>
+        <Head>
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="/static/styles/nprogress.css"
+          />
+        </Head>
+        <Header
+          errorCode={errorCode}
+          authenticated={authenticated}
+          isAdmin={isAdmin}
+          notifications={this.notifications}
+          router={router}
+        />
+        <Container>
+          <Main>{this.props.children}</Main>
+          {renderFooter && <Footer />}
+        </Container>
+      </Fragment>
     )
   }
 }
