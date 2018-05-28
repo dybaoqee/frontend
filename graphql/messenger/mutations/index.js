78620fed193726283423232bd589ac83adb91114
Nathan Queija <nathanqcam@gmail.com>
2018-05-28 16:22:30 -0300

    Add messenger mutations

--- graphql/messenger/mutations/index.js
+++ graphql/messenger/mutations/index.js
@@ -1,22 +1,17 @@
 import gql from 'graphql-tag'
 
 export const SEND_MESSAGE = gql`
-  mutation favoriteListing($listingId: ID, $message: String, $receiverId: ID!) {
+  mutation sendMessage($listingId: ID!, $message: String, $receiverId: ID!) {
     sendMessage(
       listingId: $listingId
       message: $message
       receiverId: $receiverId
     ) {
       id
-      notified
-      read
-      receiver {
-        name
-        id
-      }
+      message
+      insertedAt
       sender {
         id
-        name
       }
     }
   }
