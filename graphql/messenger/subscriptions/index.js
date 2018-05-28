9272047bcc8e584af97f9832e00af6062a8aa5a0
Nathan Queija <nathanqcam@gmail.com>
2018-05-28 16:22:42 -0300

    Ad messenger subscription

--- graphql/messenger/subscriptions/index.js
+++ graphql/messenger/subscriptions/index.js
@@ -3,9 +3,17 @@ import gql from 'graphql-tag'
 export const MESSAGE_SENT = gql`
   subscription {
     messageSent {
+      id
       message
+      notified
+      read
+      insertedAt
       sender {
+        id
         name
+        email
+        phone
+        role
       }
     }
   }
