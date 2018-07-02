281f9335284808f7185b77293c9cc71ca0decc3f
Nathan Queija <nathanqcam@gmail.com>
2018-07-02 10:56:33 -0300

    Add ContactButton component

---
+++ components/shared/Common/Contact/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  h3 {
+    font-size: 16px;
+    font-weight: 700;
+    margin: 10px;
+  }
+
+  textarea {
+    resize: vertical;
+    max-height: 180px;
+  }
+`
