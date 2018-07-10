946e7c14452297440020bf36e0cf82824fc5747a
Nathan Queija <nathanqcam@gmail.com>
2018-07-09 21:24:54 -0300

    Add property type treatment

--- utils/filter-params.js
+++ utils/filter-params.js
@@ -1,3 +1,7 @@
+import _ from 'lodash'
+
+const filterValid = (object) => _.omit(object, [undefined])
+
 function joinParam(param) {
   return param
     .map(function(item) {
@@ -10,71 +14,66 @@ function joinParam(param) {
     .join('|')
 }
 
-function treatMinPrice(params) {
-  if (params.price.min) {
-    return `preco_minimo=${params.price.min}`
-  }
+function treatMinPrice(price) {
+  if (price && price.min) return `preco_minimo=${price.min}`
 }
 
-function treatMaxPrice(params) {
-  if (params.price.max) {
-    return `preco_maximo=${params.price.max}`
-  }
+function treatMaxPrice(price) {
+  if (price && price.max) return `preco_maximo=${price.max}`
 }
 
-function treatMinArea(params) {
-  if (params.area.min) {
-    return `area_minima=${params.area.min}`
-  }
+function treatMinArea(area) {
+  if (area && area.min) return `area_minima=${area.min}`
 }
 
-function treatMaxArea(params) {
-  if (params.area.max) {
-    return `area_maxima=${params.area.max}`
-  }
+function treatMaxArea(area) {
+  if (area && area.max) return `area_maxima=${area.max}`
 }
 
-function treatMinRooms(params) {
-  if (params.rooms.min) {
-    return `quartos_minimo=${params.rooms.min}`
-  }
+function treatMinRooms(rooms) {
+  if (rooms && rooms.min) return `quartos_minimo=${rooms.min}`
 }
 
-function treatMaxRooms(params) {
-  if (params.rooms.max) {
-    return `quartos_maximo=${params.rooms.max}`
-  }
+function treatMaxRooms(rooms) {
+  if (rooms && rooms.max) return `quartos_maximo=${rooms.max}`
 }
 
-function treatMinGarageSpots(params) {
-  if (params.garageSpots.min) {
-    return `vagas_minimo=${params.garageSpots.min}`
-  }
+function treatMinGarageSpots(garageSpots) {
+  if (garageSpots) return `vagas_minimo=${garageSpots.min}`
 }
 
-function treatMaxGarageSpots(params) {
-  if (params.garageSpots.max) {
-    return `vagas_maximo=${params.garageSpots.max}`
-  }
+function treatMaxGarageSpots(garageSpots) {
+  if (garageSpots) return `vagas_maximo=${garageSpots.max}`
 }
 
-function treatNeighborhoods(params) {
-  if (params.neighborhoods.length > 0) {
-    return `bairros=${joinParam(params.neighborhoods)}`
-  }
+function treatNeighborhoods(neighborhoods) {
+  if (neighborhoods && neighborhoods.length > 0)
+    return `bairros=${joinParam(neighborhoods)}`
 }
 
-export function treatParams(params) {
+function treatPropertyType(types) {
+  if (types && types.length > 0) return `tipos=${joinParam(types)}`
+}
+
+export function treatParams({
+  price,
+  area,
+  rooms,
+  garageSpots,
+  neighborhoods,
+  types
+}) {
   return [
-    treatMinPrice(params),
-    treatMaxPrice(params),
-    treatMinArea(params),
-    treatMaxArea(params),
-    treatMinRooms(params),
-    treatMaxRooms(params),
-    treatMinGarageSpots(params),
-    treatMaxGarageSpots(params),
-    treatNeighborhoods(params)
+    treatMinPrice(price),
+    treatMaxPrice(price),
+    treatMinArea(area),
+    treatMaxArea(area),
+    treatMinRooms(rooms),
+    treatMaxRooms(rooms),
+    treatMinGarageSpots(garageSpots),
+    treatMaxGarageSpots(garageSpots),
+    treatNeighborhoods(neighborhoods),
+    treatPropertyType(types)
   ]
     .filter((n) => n)
     .join('&')
@@ -83,35 +82,65 @@ export function treatParams(params) {
 const splitParam = (param) => (param ? param.split('|') : [])
 
 export const getFiltersForGraphQL = ({
+  price,
+  area,
+  rooms,
+  garageSpots,
+  neighborhoods,
+  types
+}) =>
+  filterValid({
+    minPrice: price && price.min,
+    maxPrice: price && price.max,
+    minArea: area && area.min,
+    maxArea: area && area.max,
+    minRooms: rooms && rooms.min,
+    maxRooms: rooms && rooms.max,
+    minGarageSpots: garageSpots && garageSpots.min,
+    maxGarageSpots: garageSpots && garageSpots.max,
+    neighborhoods:
+      neighborhoods &&
+      neighborhoods.map(
+        (neighborhood) =>
+          neighborhood.value ? neighborhood.value : neighborhood
+      ),
+    types: types && types
+  })
+
+export const getDerivedParams = ({
   preco_minimo,
   preco_maximo,
   area_minima,
   area_maxima,
+  vagas_minimo,
+  vagas_maximo,
   quartos_minimo,
   quartos_maximo,
-  bairros
-}) => ({
-  minPrice: parseInt(preco_minimo),
-  maxPrice: parseInt(preco_maximo),
-  minArea: parseInt(area_minima),
-  maxArea: parseInt(area_maxima),
-  minRooms: parseInt(quartos_minimo),
-  maxRooms: parseInt(quartos_maximo),
-  neighborhoods: splitParam(bairros)
-})
-
-export const getDerivedParams = (query) => ({
-  price: {
-    min: query.preco_minimo,
-    max: query.preco_maximo
-  },
-  area: {
-    min: query.area_minima,
-    max: query.area_maxima
-  },
-  rooms: {
-    min: query.quartos_minimo,
-    max: query.quartos_maximo
-  },
-  neighborhoods: splitParam(query.bairros)
-})
+  bairros,
+  tipos
+}) => {
+  return filterValid({
+    [preco_minimo && preco_maximo ? 'price' : undefined]: {
+      min: parseInt(preco_minimo),
+      max: parseInt(preco_maximo)
+    },
+    [area_minima && area_maxima ? 'area' : undefined]: {
+      min: parseInt(area_minima),
+      max: parseInt(area_maxima)
+    },
+    [quartos_minimo && quartos_maximo ? 'rooms' : undefined]: {
+      min: parseInt(quartos_minimo),
+      max: parseInt(quartos_maximo)
+    },
+    [vagas_minimo && vagas_maximo ? 'garageSpots' : undefined]: {
+      min: parseInt(vagas_minimo),
+      max: parseInt(vagas_maximo)
+    },
+    [bairros && splitParam(bairros).length > 0
+      ? 'neighborhoods'
+      : undefined]: splitParam(bairros),
+    [tipos && splitParam(tipos).length > 0 ? 'types' : undefined]: splitParam(
+      tipos
+    )
+  })
+}
