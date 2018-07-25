e0aadf0671ae61ecae1d362007d2bacaf89d3ed8
Nathan Queija <nathanqcam@gmail.com>
2018-07-25 11:09:30 -0300

    Remove unused code

--- components/shared/PriceEstimate/index.js
+++ components/shared/PriceEstimate/index.js
@@ -1,58 +1,176 @@
-import {Component} from 'react'
-import Slider from 'components/shared/Common/Slider'
-import NumberFormat from 'react-number-format'
-import Container, {
-  SliderContainer,
-  Description,
-  InfoContainer,
-  Info,
-  Table,
-  DescriptionColumn,
-  Column,
-  Buttons
-} from './styles'
-import Link from 'next/link'
-import EmCasaButton from 'components/shared/Common/Buttons'
-import ContactButton from 'components/shared/Common/Contact'
+import React, {Component} from 'react'
+import Container, {StepContainer, Buttons} from './styles'
 import Stepper from 'components/shared/Common/Stepper'
+import EmCasaButton from 'components/shared/Common/Buttons'
+import {filterComponent} from 'services/google-maps-api'
+import AddressAutoComplete from './steps/AddressAutoComplete'
+import PropertyInfo from './steps/PropertyInfo'
+import UserInfo from './steps/UserInfo'
+import EstimateSuccess from './steps/EstimateSuccess'
+import OutOfService from './steps/OutOfService'
+import EstimateFail from './steps/EstimateFail'
+import {isEmailValid} from 'lib/validation'
+import {Mutation} from 'react-apollo'
+import {ESTIMATE_PRICE} from 'graphql/listings/mutations'
+import {getNeighborhoods} from 'services/neighborhood-api'
 
 export default class PriceEstimate extends Component {
   state = {
-    listingValue: 400,
-    used: false
+    step: 1,
+    canAdvance: false,
+    loading: false,
+    neighborhoods: [],
+    listing: {
+      area: 0,
+      rooms: 0,
+      garageSpots: 0,
+      bathrooms: 0,
+      name: '',
+      email: ''
+    }
   }
-  sliderChanged = ({maxValue}, userClicked) => {
-    const {used} = this.state
-    this.setState({listingValue: maxValue, used: userClicked})
 
-    if (!used && userClicked) {
-      window.dataLayer.push({
-        action: 'User used calculator',
-        event: 'user_used_calculator'
+  async componentDidMount() {
+    let neighborhoods = await getNeighborhoods().then(
+      ({data}) => data.neighborhoods
+    )
+    this.setState({neighborhoods})
+  }
+
+  get buttonTexts() {
+    return ['Avaliar', 'Próximo', 'Quanto vale meu imóvel?']
+  }
+
+  setChosenPlace = (placeChosen) => {
+    const {listing} = this.state
+    const {address_components: components} = placeChosen
+    const neighborhood = filterComponent(components, 'sublocality_level_1')
+      .long_name
+    const street = filterComponent(components, 'route').long_name
+    const streetNumber = filterComponent(components, 'street_number').long_name
+    const state = filterComponent(components, 'administrative_area_level_1')
+      .short_name
+    const city = filterComponent(components, 'administrative_area_level_2')
+      .long_name
+    const postalCode = filterComponent(components, 'postal_code').long_name
+
+    this.setState({
+      canAdvance: true,
+      listing: {
+        ...listing,
+        address: {
+          city,
+          lat: placeChosen.geometry.location.lat,
+          lng: placeChosen.geometry.location.lng,
+          neighborhood,
+          postalCode,
+          street,
+          streetNumber,
+          state
+        }
+      }
+    })
+  }
+
+  get steps() {
+    return [
+      <AddressAutoComplete />,
+      <PropertyInfo />,
+      <UserInfo />,
+      <EstimateSuccess />,
+      <OutOfService />,
+      <EstimateFail />
+    ]
+  }
+
+  getStepContent = (page) => {
+    const {listing} = this.state
+    const Current = this.steps[page]
+    return React.cloneElement(Current, {
+      choosePlace: this.setChosenPlace,
+      onChange: this.onFieldChange,
+      listing
+    })
+  }
+
+  nextPage = async (getSuggestedPrice) => {
+    const {
+      neighborhoods,
+      step,
+      listing: {address, name, email, area, rooms, garageSpots, bathrooms}
+    } = this.state
+
+    if (step < 3) {
+      this.setState({
+        step: step + 1,
+        canAdvance: false
+      })
+    } else {
+      if (neighborhoods.indexOf(address.neighborhood) === -1) {
+        this.setState({
+          step: 5,
+          canAdvance: false
+        })
+
+        return
+      }
+      const {data: {requestPriceSuggestion}} = await getSuggestedPrice({
+        variables: {address, name, email, area, rooms, garageSpots, bathrooms}
+      })
+
+      const nextStep = requestPriceSuggestion.suggestedPrice ? 4 : 6
+
+      this.setState({
+        listing: {
+          ...this.state.listing,
+          suggestedPrice: requestPriceSuggestion.suggestedPrice
+        },
+        step: nextStep,
+        canAdvance: false
       })
     }
   }
 
-  getSellerEconomy = () => {
-    const {listingValue} = this.state
-    return Math.round(1.2 / 100 * listingValue + 3400)
+  onFieldChange = (e) => {
+    const {listing, step} = this.state
+    let canAdvance = false
+    const newListing = {...listing}
+    newListing[e.target.name] = parseInt(e.target.value) || e.target.value
+
+    if (step === 2) {
+      canAdvance =
+        newListing.area > 0 && newListing.bathrooms > 0 && newListing.rooms > 0
+    } else if (step == 3) {
+      canAdvance = newListing.name.length > 5 && isEmailValid(newListing.email)
+    }
+
+    this.setState({listing: newListing, canAdvance})
   }
 
-  getBuyerEconomy = () => {
-    const {listingValue} = this.state
-    return Math.round(0.8 / 100 * listingValue + 2400)
-  }
   render() {
-    const {listingValue} = this.state
-    return (
-      <Container>
-        <h3>Quer saber quanto vale seu imóvel?</h3>
-        <Stepper steps={3} current={1} />
+    const {step, canAdvance} = this.state
+    const {buttonTexts, nextPage, getStepContent} = this
 
-        <Buttons>
-          <ContactButton />
-        </Buttons>
-      </Container>
+    return (
+      <Mutation mutation={ESTIMATE_PRICE}>
+        {(getSuggestedPrice, {loading}) => (
+          <Container>
+            <h3>Quer saber quanto vale seu imóvel?</h3>
+            <Stepper steps={4} current={step <= 4 ? step : 4} />
+            <StepContainer>{getStepContent(step - 1)}</StepContainer>
+            {step < 4 && (
+              <Buttons>
+                <EmCasaButton
+                  disabled={!canAdvance || loading}
+                  onClick={nextPage.bind(this, getSuggestedPrice)}
+                >
+                  {loading ? 'Calculando...' : buttonTexts[step - 1]}
+                </EmCasaButton>
+              </Buttons>
+            )}
+          </Container>
+        )}
+      </Mutation>
     )
   }
 }
