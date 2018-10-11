92b0561b50c406bf6b3568bf816aa9494f2b4859
Lucas Tulio <lucasctulio@gmail.com>
2018-10-11 19:31:39 -0300

    Address screen inputs and navigation

--- components/listings/new-listing/steps/AddressInput/index.js
+++ components/listings/new-listing/steps/AddressInput/index.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react'
+import Button from '@emcasa/ui-dom/components/Button'
+import Row from '@emcasa/ui-dom/components/Row'
+import Col from '@emcasa/ui-dom/components/Col'
+import View from '@emcasa/ui-dom/components/View'
+import Text from '@emcasa/ui-dom/components/Text'
+import Input from '@emcasa/ui-dom/components/Input'
 
 class AddressInput extends Component {
   render() {
     return (
       <div ref={this.props.hostRef}>
-        <p>address input screen</p>
+        <Row justifyContent="center">
+          <Col width={[1, 1/2]}>
+            <View body p={4}>
+              <Text
+                fontSize="large"
+                fontWeight="bold"
+                textAlign="center"
+              >
+                Qual o endereço do seu imóvel?
+              </Text>
+              <Col width={1} mb={4} style={{width: '100%'}}>
+                <Input placeholder="Endereço e número*" />
+              </Col>
+              <Col width={1}>
+                <Input placeholder="Complemento" />
+              </Col>
+            </View>
+            <View bottom p={4}>
+              <Row justifyContent="space-between">
+                <Col width={5/12}>
+                  <Button
+                    fluid
+                    height="tall"
+                    onClick={this.props.previousStep}>Cancelar</Button>
+                </Col>
+                <Col width={5/12}>
+                  <Button
+                    fluid
+                    height="tall"
+                    disabled
+                    onClick={this.props.nextStep}>Avançar</Button>
+                </Col>
+              </Row>
+            </View>
+          </Col>
+        </Row>
       </div>
     )
   }
--- components/listings/new-listing/steps/intro/index.js
+++ components/listings/new-listing/steps/intro/index.js
@@ -10,46 +10,46 @@ class Intro extends Component {
   render() {
     return (
       <div ref={this.props.hostRef}>
-      <Row justifyContent="center" ref={this.props.hostRef}>
-        <Col width={[1, 1/2]}>
-        <View body p={4}>
-          <Text
-            fontSize="large"
-            fontWeight="bold"
-            textAlign="center"
-          >
-            Como funciona anunciar aqui na EmCasa?
-          </Text>
-          <Text color="grey">Venda seu imóvel do jeito fácil e seguro.</Text>
-          <View>
-            <Arrow /><Text inline>Cadastre seu imóvel no nosso site</Text>
-          </View>
-          <View>
-            <Arrow /><Text inline>Agende grátis um Tour Virtual 3D e uma sessão de fotos do seu imóvel</Text>
-          </View>
-          <View>
-            <Arrow /><Text inline>Economize tempo e dinheiro anunciando seu imóvel com a gente</Text>
-          </View>
-          <View>
-            <Arrow /><Text inline>Pré avaliação grátis do seu imóvel</Text>
-          </View>
-          <View>
-            <Arrow /><Text inline>Assistência jurídica grátis</Text>
-          </View>
-        </View>
         <Row justifyContent="center">
           <Col width={[1, 1/2]}>
-            <View bottom p={4}>
-              <Button
-                fluid
-                active
-                height="tall"
-                onClick={this.props.nextStep}>Quero Anunciar</Button>
+            <View body p={4}>
+              <Text
+                fontSize="large"
+                fontWeight="bold"
+                textAlign="center"
+              >
+                Como funciona anunciar aqui na EmCasa?
+              </Text>
+              <Text color="grey">Venda seu imóvel do jeito fácil e seguro.</Text>
+              <View>
+                <Arrow /><Text inline>Cadastre seu imóvel no nosso site</Text>
+              </View>
+              <View>
+                <Arrow /><Text inline>Agende grátis um Tour Virtual 3D e uma sessão de fotos do seu imóvel</Text>
+              </View>
+              <View>
+                <Arrow /><Text inline>Economize tempo e dinheiro anunciando seu imóvel com a gente</Text>
+              </View>
+              <View>
+                <Arrow /><Text inline>Pré avaliação grátis do seu imóvel</Text>
+              </View>
+              <View>
+                <Arrow /><Text inline>Assistência jurídica grátis</Text>
+              </View>
             </View>
+            <Row justifyContent="center">
+              <Col width={[1, 1/2]}>
+                <View bottom p={4}>
+                  <Button
+                    fluid
+                    active
+                    height="tall"
+                    onClick={this.props.nextStep}>Quero Anunciar</Button>
+                </View>
+              </Col>
+            </Row>
           </Col>
         </Row>
-        </Col>
-      </Row>
       </div>
     )
   }
