9ec78b58be19ed36dbc80163d20a666a29e06de5
Lucas Tulio <lucasctulio@gmail.com>
2018-10-18 00:25:36 -0300

    Home details form

--- components/listings/new-listing/steps/HomeDetails/index.js
+++ components/listings/new-listing/steps/HomeDetails/index.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
+import { Formik, Field } from 'formik'
+
 import Button from '@emcasa/ui-dom/components/Button'
 import Input from '@emcasa/ui-dom/components/Input'
 import Row from '@emcasa/ui-dom/components/Row'
@@ -6,7 +8,7 @@ import Col from '@emcasa/ui-dom/components/Col'
 import View from '@emcasa/ui-dom/components/View'
 import Text from '@emcasa/ui-dom/components/Text'
 
-class HomeDetails extends Component {
+class HomeDetails extends PureComponent {
   constructor(props) {
     super(props)
     this.nextStep = this.nextStep.bind(this)
@@ -27,51 +29,78 @@ class HomeDetails extends Component {
       <div ref={this.props.hostRef}>
         <Row justifyContent="center">
           <Col width={[1, 1/2]}>
-            <View body p={4}>
-              <Text
-                fontSize="large"
-                fontWeight="bold"
-                textAlign="center"
-              >
-                Por favor, informe os detalhes do seu imóvel
-              </Text>
-              <Text color="grey">Com base nos detalhes do seu imóvel, calcularemos um valor médio de venda.</Text>
-              <Col mb={4} mr={4}>
-                <Input placeholder="Tipo do Imóvel*" />
-              </Col>
-              <Row mb={4}>
-                <Col width={1/2} mr={4}>
-                  <Input placeholder="Nº andar" />
-                </Col>
-                <Col width={1/2} ml={2} mr={4}>
-                  <Input label="Área conforme IPTU*" placeholder="Área (m²)*"/>
-                </Col>
-              </Row>
-              <Row mb={4}>
-                <Col width={1/2} mr={4}>
-                  <Input placeholder="Cond (R$)" />
-                </Col>
-                <Col width={1/2} ml={2} mr={4}>
-                  <Input placeholder="IPTU (R$/ano)*"/>
-                </Col>
-              </Row>
-            </View>
-            <View bottom p={4}>
-              <Row justifyContent="space-between">
-                <Col width={5/12}>
-                  <Button
-                    fluid
-                    height="tall"
-                    onClick={this.previousStep}>Voltar</Button>
-                </Col>
-                <Col width={5/12}>
-                  <Button
-                    fluid
-                    height="tall"
-                    onClick={this.nextStep}>Avançar</Button>
-                </Col>
-              </Row>
-            </View>
+            <Formik
+              render={({isValid}) => (
+                <>
+                  <View body p={4}>
+                    <Text
+                      fontSize="large"
+                      fontWeight="bold"
+                      textAlign="center"
+                    >
+                      Por favor, informe os detalhes do seu imóvel
+                    </Text>
+                    <Text color="grey">Com base nos detalhes do seu imóvel, calcularemos um valor médio de venda.</Text>
+                    <Col mb={4} mr={4}>
+                      <Field
+                        name="homeType"
+                        render={({field}) => (
+                          <Input {...field} placeholder="Tipo do Imóvel*" />
+                        )}/>
+                    </Col>
+                    <Row mb={4}>
+                      <Col width={1/2} mr={4}>
+                        <Field
+                          name="floor"
+                          render={({field}) => (
+                            <Input {...field} placeholder="Nº andar" type="number" />
+                          )}/>
+                      </Col>
+                      <Col width={1/2} ml={2} mr={4}>
+                        <Field
+                          name="area"
+                          render={({field}) => (
+                            <Input {...field} label="Área conforme IPTU*" placeholder="Área (m²)*" type="number" />
+                          )}/>
+                      </Col>
+                    </Row>
+                    <Row mb={4}>
+                      <Col width={1/2} mr={4}>
+                        <Field
+                          name="cond"
+                          render={({field}) => (
+                            <Input {...field} placeholder="Cond (R$)" />
+                          )}/>
+                      </Col>
+                      <Col width={1/2} ml={2} mr={4}>
+                        <Field
+                          name="iptu"
+                          render={({field}) => (
+                            <Input {...field} placeholder="IPTU (R$/ano)*"/>
+                          )}/>
+                      </Col>
+                    </Row>
+                  </View>
+                  <View bottom p={4}>
+                    <Row justifyContent="space-between">
+                      <Col width={5/12}>
+                        <Button
+                          fluid
+                          height="tall"
+                          onClick={this.previousStep}>Voltar</Button>
+                      </Col>
+                      <Col width={5/12}>
+                        <Button
+                          fluid
+                          height="tall"
+                          disabled={!isValid}
+                          onClick={this.nextStep}>Avançar</Button>
+                      </Col>
+                    </Row>
+                  </View>
+                </>
+              )}
+            />
           </Col>
         </Row>
       </div>
