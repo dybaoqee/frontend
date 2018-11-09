567799bc9e9898e705fa5ccada158bc9fc114d3a
Lucas Tulio <lucasctulio@gmail.com>
2018-11-09 13:12:37 -0200

    Add Intro and AddressInput tests

---
+++ __tests__/components/listings/new-listing/steps/AddressInput/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AddressInput from 'components/listings/new-listing/steps/AddressInput';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components'
+import theme from '@emcasa/ui'
+import { ThemeProvider } from 'styled-components'
+
+describe('<AddressInput/>', () => {
+  it('should render a clean page', () => {
+    const tree = renderer.create(
+      <ThemeProvider theme={theme}>
+        <AddressInput />
+      </ThemeProvider>).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
