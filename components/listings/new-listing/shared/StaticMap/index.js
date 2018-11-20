3018f3c863ba91074fab0a27fc82fded51779f2b
Gabriel Laet <gabriel.laet@gmail.com>
2018-11-20 17:53:56 -0200

    Adding custom marker image for StaticMap

--- components/listings/new-listing/shared/StaticMap/index.js
+++ components/listings/new-listing/shared/StaticMap/index.js
@@ -1,19 +1,20 @@
-import React, { PureComponent } from 'react'
-import { key } from 'services/google-maps-api'
+import {PureComponent} from 'react'
+import {key} from 'services/google-maps-api'
 import StyledImage from './styles'
 
 const ZOOM = 15
 const MAP_HEIGHT = 110
+const MARKER_URL = 'https://s3.amazonaws.com/emcasa-ui/images/place-48x48px.png'
 
 class StaticMap extends PureComponent {
   render() {
-    const { addressData } = this.props
+    const {addressData} = this.props
     if (!addressData) {
       return null
     }
     const mapWidth = window.innerWidth
-    const { geometry: { location: { lat, lng }}} = addressData
-    const params = `zoom=${ZOOM}&size=${mapWidth}x${MAP_HEIGHT}&scale=2&maptype=roadmap&markers=color:red%7C${lat},${lng}&style=feature:water%7Ccolor:0xa0d5ff&style=feature:`
+    const {geometry: {location: {lat, lng}}} = addressData
+    const params = `zoom=${ZOOM}&size=${mapWidth}x${MAP_HEIGHT}&scale=2&maptype=roadmap&markers=scale:2|icon:${MARKER_URL}|${lat},${lng}&style=feature:water|color:0xa0d5ff&style=feature:`
     const endpoint = `https://maps.googleapis.com/maps/api/staticmap?${params}&key=${key}`
     return <StyledImage src={endpoint} />
   }
