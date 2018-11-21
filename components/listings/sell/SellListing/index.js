1c6420289b6b8333ad683f50a54bb8f548fea11e
Gabriel Laet <gabriel.laet@gmail.com>
2018-11-21 15:52:24 -0200

    Initial content for seller landing page

--- components/listings/sell/SellListing/index.js
+++ components/listings/sell/SellListing/index.js
@@ -1,7 +1,37 @@
 import {Component} from 'react'
+import View from '@emcasa/ui-dom/components/View'
+import Col from '@emcasa/ui-dom/components/Col'
+import Row from '@emcasa/ui-dom/components/Row'
+import Text from '@emcasa/ui-dom/components/Text'
+import Input from '@emcasa/ui-dom/components/Input'
+import Button from '@emcasa/ui-dom/components/Button'
 
 export default class SellListing extends Component {
   render() {
-    return <h1>Quer vender seu imóvel?</h1>
+    return (
+      <View>
+        <Row>
+          <Col>
+            <Text fontSize="xlarge" color="dark">
+              Quer vender seu imóvel?
+            </Text>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Text fontSize="large" color="gray">
+              Vender seu imóvel com a EmCasa nunca foi tão fácil, informe o endereço do seu imóvel
+              e faremos uma pré-avaliação do valor para você
+            </Text>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Input />
+            <Button active>Avaliar</Button>
+          </Col>
+        </Row>
+      </View>
+    )
   }
 }
--- pages/listings/sell/index.js
+++ pages/listings/sell/index.js
@@ -1,17 +1,29 @@
+import '@emcasa/ui-dom/components/global-styles'
 import {Component} from 'react'
+import theme from '@emcasa/ui'
+import View from '@emcasa/ui-dom/components/View'
+import {ThemeProvider} from 'styled-components'
 import SellListing from 'components/listings/sell/SellListing'
 import Benefits from 'components/listings/sell/Benefits'
 import HowItWorks from 'components/listings/sell/HowItWorks'
-import View from '@emcasa/ui-dom/components/View'
 
 export default class Sell extends Component {
+  static async getInitialProps() {
+    return {
+      shortLogo: true,
+      hideSeparator: true
+    }
+  }
+
   render() {
     return (
-      <View>
-        <SellListing />
-        <Benefits />
-        <HowItWorks />
-      </View>
+      <ThemeProvider theme={theme}>
+        <View>
+          <SellListing />
+          <Benefits />
+          <HowItWorks />
+        </View>
+      </ThemeProvider>
     )
   }
 }
