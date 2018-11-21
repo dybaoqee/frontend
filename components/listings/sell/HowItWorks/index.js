0cb529d75b2211f4038cab6ae8a3083902c16fe3
Gabriel Laet <gabriel.laet@gmail.com>
2018-11-21 18:23:47 -0200

    Adding styling to HowItWorks block on seller landing page

--- components/listings/sell/HowItWorks/index.js
+++ components/listings/sell/HowItWorks/index.js
@@ -1,59 +1,73 @@
 import {Component} from 'react'
+import styled from 'styled-components'
 import View from '@emcasa/ui-dom/components/View'
 import Col from '@emcasa/ui-dom/components/Col'
 import Row from '@emcasa/ui-dom/components/Row'
 import Text from '@emcasa/ui-dom/components/Text'
 import Button from '@emcasa/ui-dom/components/Button'
 
+const Container = styled(View)`
+  display: flex;
+  justify-content: center;
+`
+
+const Content = styled(View)`
+  min-width: 1100px;
+`
+
 export default class HowItWorks extends Component {
   render() {
     return (
-      <View>
-        <Row>
-          <Col>
-            <Text fontSize="large" color="dark">
-              100% de Satisfação Garantida
-            </Text>
-          </Col>
-        </Row>
-        <Row>
-          <Col width={2 / 12}>
-            <Text fontSize="medium" color="dark">
-              Cadastro do Imóvel
-            </Text>
-          </Col>
-          <Col width={2 / 12}>
-            <Text fontSize="medium" color="dark">
-              Avaliação precisa do seu imóvel
-            </Text>
-          </Col>
-          <Col width={2 / 12}>
-            <Text fontSize="medium" color="dark">
-              Tour Virtual 3D e Fotos Profissionais
-            </Text>
-          </Col>
-          <Col width={2 / 12}>
-            <Text fontSize="medium" color="dark">
-              Imóvel Publicado
-            </Text>
-          </Col>
-          <Col width={2 / 12}>
-            <Text fontSize="medium" color="dark">
-              Visitas Agendadas
-            </Text>
-          </Col>
-          <Col width={2 / 12}>
-            <Text fontSize="medium" color="dark">
-              Venda finalizada
-            </Text>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Button>Quero vender meu imóvel</Button>
-          </Col>
-        </Row>
-      </View>
+      <Container>
+        <Content>
+          <Row justifyContent="center">
+            <Col>
+              <Text fontSize="xlarge" color="dark" fontWeight="bold">
+                100% de Satisfação Garantida
+              </Text>
+            </Col>
+          </Row>
+          <Row justifyContent="center">
+            <Col width={2 / 12}>
+              <Text fontSize="medium" color="dark">
+                Cadastro do Imóvel
+              </Text>
+            </Col>
+            <Col width={2 / 12}>
+              <Text fontSize="medium" color="dark">
+                Avaliação precisa do seu imóvel
+              </Text>
+            </Col>
+            <Col width={2 / 12}>
+              <Text fontSize="medium" color="dark">
+                Tour Virtual 3D e Fotos Profissionais
+              </Text>
+            </Col>
+            <Col width={2 / 12}>
+              <Text fontSize="medium" color="dark">
+                Imóvel Publicado
+              </Text>
+            </Col>
+            <Col width={2 / 12}>
+              <Text fontSize="medium" color="dark">
+                Visitas Agendadas
+              </Text>
+            </Col>
+            <Col width={2 / 12}>
+              <Text fontSize="medium" color="dark">
+                Venda finalizada
+              </Text>
+            </Col>
+          </Row>
+          <Row justifyContent="center">
+            <Col width={4 / 12}>
+              <Button height="tall" fluid>
+                Quero vender meu imóvel
+              </Button>
+            </Col>
+          </Row>
+        </Content>
+      </Container>
     )
   }
 }
