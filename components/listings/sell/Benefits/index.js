556016a680afa37f9aa1eb743e6b6c8f236a05c7
Gabriel Laet <gabriel.laet@gmail.com>
2018-11-21 18:05:36 -0200

    Styling Benefits block on seller landing page

--- components/listings/sell/Benefits/index.js
+++ components/listings/sell/Benefits/index.js
@@ -1,54 +1,68 @@
 import {Component} from 'react'
+import styled from 'styled-components'
 import View from '@emcasa/ui-dom/components/View'
 import Col from '@emcasa/ui-dom/components/Col'
 import Row from '@emcasa/ui-dom/components/Row'
 import Text from '@emcasa/ui-dom/components/Text'
 import Button from '@emcasa/ui-dom/components/Button'
 
+const Container = styled(View)`
+  display: flex;
+  justify-content: center;
+`
+
+const Content = styled(View)`
+  max-width: 1100px;
+`
+
 export default class Benefits extends Component {
   render() {
     return (
-      <View>
-        <Row>
-          <Col>
-            <Text fontSize="large" color="dark">
-              Conheça as vantagens de vender com a EmCasa!
-            </Text>
-          </Col>
-        </Row>
-        <Row>
-          <Col width={3 / 12}>
-            <Text fontSize="xlarge" color="dark">
-              Tour Virtual 3D
-            </Text>
-            <Text color="gray">
-              O Tour Virtual 3D permite que as pessoas vejam o imóvel de uma
-              maneira diferenciada
-            </Text>
-          </Col>
-          <Col width={3 / 12}>
-            <Text fontSize="xlarge" color="dark">
-              Avaliação precisa
-            </Text>
-            <Text color="gray">
-              Nossa plataforma calcula automaticamente o valor do seu imóvel
-            </Text>
-          </Col>
-          <Col width={3 / 12}>
-            <Text fontSize="xlarge" color="dark">
-              Assitência jurídica
-            </Text>
-            <Text color="gray">
-              Aqui você tem Assistência Jurídica grátis com documentação e processos
-            </Text>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Button>Vender meu imóvel</Button>
-          </Col>
-        </Row>
-      </View>
+      <Container>
+        <Content>
+          <Row justifyContent="center">
+            <Col>
+              <Text fontSize="large" color="dark" fontWeight="bold">
+                Conheça as vantagens de vender com a EmCasa!
+              </Text>
+            </Col>
+          </Row>
+          <Row justifyContent="center">
+            <Col width={3 / 12}>
+              <Text fontSize="large" color="dark">
+                Tour Virtual 3D
+              </Text>
+              <Text color="gray">
+                O Tour Virtual 3D permite que as pessoas vejam o imóvel de uma
+                maneira diferenciada
+              </Text>
+            </Col>
+            <Col width={3 / 12}>
+              <Text fontSize="large" color="dark">
+                Avaliação precisa
+              </Text>
+              <Text color="gray">
+                Nossa plataforma calcula automaticamente o valor do seu imóvel
+              </Text>
+            </Col>
+            <Col width={3 / 12}>
+              <Text fontSize="large" color="dark">
+                Assitência jurídica
+              </Text>
+              <Text color="gray">
+                Aqui você tem Assistência Jurídica grátis com documentação e processos
+              </Text>
+            </Col>
+          </Row>
+          <Row justifyContent="center">
+            <Col width={4 / 12}>
+              <Button height="tall" fluid>
+                Vender meu imóvel
+              </Button>
+            </Col>
+          </Row>
+        </Content>
+      </Container>
     )
   }
 }
