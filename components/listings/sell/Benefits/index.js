8958314c5259de97da489f846913b06e62c376f5
Gabriel Laet <gabriel.laet@gmail.com>
2018-11-21 16:47:53 -0200

    Adding more content to seller landing page

--- components/listings/sell/Benefits/index.js
+++ components/listings/sell/Benefits/index.js
@@ -1,7 +1,54 @@
 import {Component} from 'react'
+import View from '@emcasa/ui-dom/components/View'
+import Col from '@emcasa/ui-dom/components/Col'
+import Row from '@emcasa/ui-dom/components/Row'
+import Text from '@emcasa/ui-dom/components/Text'
+import Button from '@emcasa/ui-dom/components/Button'
 
 export default class Benefits extends Component {
   render() {
-    return <h1>Vantagens</h1>
+    return (
+      <View>
+        <Row>
+          <Col>
+            <Text fontSize="large" color="dark">
+              Conheça as vantagens de vender com a EmCasa!
+            </Text>
+          </Col>
+        </Row>
+        <Row>
+          <Col width={3 / 12}>
+            <Text fontSize="xlarge" color="dark">
+              Tour Virtual 3D
+            </Text>
+            <Text color="gray">
+              O Tour Virtual 3D permite que as pessoas vejam o imóvel de uma
+              maneira diferenciada
+            </Text>
+          </Col>
+          <Col width={3 / 12}>
+            <Text fontSize="xlarge" color="dark">
+              Avaliação precisa
+            </Text>
+            <Text color="gray">
+              Nossa plataforma calcula automaticamente o valor do seu imóvel
+            </Text>
+          </Col>
+          <Col width={3 / 12}>
+            <Text fontSize="xlarge" color="dark">
+              Assitência jurídica
+            </Text>
+            <Text color="gray">
+              Aqui você tem Assistência Jurídica grátis com documentação e processos
+            </Text>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button>Vender meu imóvel</Button>
+          </Col>
+        </Row>
+      </View>
+    )
   }
 }
--- components/listings/sell/HowItWorks/index.js
+++ components/listings/sell/HowItWorks/index.js
@@ -1,7 +1,59 @@
 import {Component} from 'react'
+import View from '@emcasa/ui-dom/components/View'
+import Col from '@emcasa/ui-dom/components/Col'
+import Row from '@emcasa/ui-dom/components/Row'
+import Text from '@emcasa/ui-dom/components/Text'
+import Button from '@emcasa/ui-dom/components/Button'
 
 export default class HowItWorks extends Component {
   render() {
-    return <h1>100% de Satisfação Garantida</h1>
+    return (
+      <View>
+        <Row>
+          <Col>
+            <Text fontSize="large" color="dark">
+              100% de Satisfação Garantida
+            </Text>
+          </Col>
+        </Row>
+        <Row>
+          <Col width={2 / 12}>
+            <Text fontSize="medium" color="dark">
+              Cadastro do Imóvel
+            </Text>
+          </Col>
+          <Col width={2 / 12}>
+            <Text fontSize="medium" color="dark">
+              Avaliação precisa do seu imóvel
+            </Text>
+          </Col>
+          <Col width={2 / 12}>
+            <Text fontSize="medium" color="dark">
+              Tour Virtual 3D e Fotos Profissionais
+            </Text>
+          </Col>
+          <Col width={2 / 12}>
+            <Text fontSize="medium" color="dark">
+              Imóvel Publicado
+            </Text>
+          </Col>
+          <Col width={2 / 12}>
+            <Text fontSize="medium" color="dark">
+              Visitas Agendadas
+            </Text>
+          </Col>
+          <Col width={2 / 12}>
+            <Text fontSize="medium" color="dark">
+              Venda finalizada
+            </Text>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button>Quero vender meu imóvel</Button>
+          </Col>
+        </Row>
+      </View>
+    )
   }
 }
