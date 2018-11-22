3722e91ae91ad7120ec885f82d71c1cb06143d4a
Gabriel Laet <gabriel.laet@gmail.com>
2018-11-22 19:18:25 -0200

    Seller Landing Page: Initial Responsive behavior

--- components/listings/new-listing/lib/mobile.js
+++ components/listings/new-listing/lib/mobile.js
@@ -3,7 +3,10 @@ import theme from '@emcasa/ui'
 const MOBILE_BREAKPOINT_WIDTH = parseInt(theme.breakpoints[0])
 
 const isMobile = () => {
-  return window.innerWidth <= MOBILE_BREAKPOINT_WIDTH
+  if (!process.browser) {
+    return false
+  }
+  return screen.width <= MOBILE_BREAKPOINT_WIDTH
 }
 
 export {
--- components/listings/sell/Benefits/index.js
+++ components/listings/sell/Benefits/index.js
@@ -1,4 +1,7 @@
 import {Component} from 'react'
+import theme from '@emcasa/ui'
+import Carousel from 'nuka-carousel'
+import NoSSR from 'react-no-ssr'
 import Link from 'next/link'
 import styled from 'styled-components'
 import View from '@emcasa/ui-dom/components/View'
@@ -6,6 +9,7 @@ import Col from '@emcasa/ui-dom/components/Col'
 import Row from '@emcasa/ui-dom/components/Row'
 import Text from '@emcasa/ui-dom/components/Text'
 import Button from '@emcasa/ui-dom/components/Button'
+import {isMobile} from 'components/listings/new-listing/lib/mobile'
 
 const Container = styled(View)`
   display: flex;
@@ -17,6 +21,10 @@ const Content = styled(View)`
   flex-direction: column;
   justify-content: space-between;
   max-width: 1100px;
+  @media (max-width: ${theme.breakpoints[0]}) {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
 `
 
 const Icon = styled.div`
@@ -37,7 +45,58 @@ const BenefitCol = styled(Col)`
     margin-bottom: 8px;
     margin-top: 0;
   }
+  @media (max-width: ${theme.breakpoints[0]}) {
+    padding-bottom: 50px;
+  }
 `
+
+const BENEFITS = [
+  {
+    icon: 'tour-3d',
+    title: 'Tour Virtual 3D',
+    description:
+      'O Tour Virtual 3D permite que as pessoas vejam o imóvel de uma maneira diferenciada'
+  },
+  {
+    icon: 'avaliacao-precisa',
+    title: 'Avaliação precisa',
+    description:
+      'Nossa plataforma calcula automaticamente o valor do seu imóvel'
+  },
+  {
+    icon: 'assistencia-juridica',
+    title: 'Assistência Jurídica',
+    description:
+      'Aqui você tem Assistência Jurídica grátis com documentação e processos'
+  }
+]
+
+const getBenefits = (ignoreMobile) => {
+  const benefits = BENEFITS.map(({icon, title, description}) => (
+    <BenefitCol width={[1, 3 / 12]}>
+      <Icon name={icon} />
+      <Text fontSize="large" color="dark">
+        {title}
+      </Text>
+      <Text color="gray">{description}</Text>
+    </BenefitCol>
+  ))
+
+  if (!ignoreMobile && isMobile()) {
+    return (
+      <Carousel
+        renderCenterLeftControls={() => null}
+        renderCenterRightControls={() => null}
+        width="300px"
+        initialSlideHeight={0}
+        heightMode="max">
+        {benefits}
+      </Carousel>
+    )
+  }
+
+  return benefits
+}
 
 export default class Benefits extends Component {
   render() {
@@ -46,44 +105,22 @@ export default class Benefits extends Component {
         <Content>
           <Row justifyContent="center">
             <Col>
-              <Text fontSize="large" color="dark" fontWeight="bold">
+              <Text fontSize="large" color="dark" fontWeight="bold" textAlign="center">
                 Conheça as vantagens de vender com a EmCasa!
               </Text>
             </Col>
           </Row>
-          <Row justifyContent="center">
-            <BenefitCol width={3 / 12}>
-              <Icon name="tour-3d" />
-              <Text fontSize="large" color="dark">
-                Tour Virtual 3D
-              </Text>
-              <Text color="gray">
-                O Tour Virtual 3D permite que as pessoas vejam o imóvel de uma
-                maneira diferenciada
-              </Text>
-            </BenefitCol>
-            <BenefitCol width={3 / 12}>
-              <Icon name="avaliacao-precisa" />
-              <Text fontSize="large" color="dark">
-                Avaliação precisa
-              </Text>
-              <Text color="gray">
-                Nossa plataforma calcula automaticamente o valor do seu imóvel
-              </Text>
-            </BenefitCol>
-            <BenefitCol width={3 / 12}>
-              <Icon name="assistencia-juridica" />
-              <Text fontSize="large" color="dark">
-                Assitência jurídica
-              </Text>
-              <Text color="gray">
-                Aqui você tem Assistência Jurídica grátis com documentação e
-                processos
-              </Text>
-            </BenefitCol>
+          <Row
+            justifyContent="center"
+            flexDirection={['column', 'row']}
+            alignItems={['center', 'none']}
+          >
+            <NoSSR onSSR={getBenefits(true)}>
+              {getBenefits()}
+            </NoSSR>
           </Row>
           <Row justifyContent="center">
-            <Col width={4 / 12}>
+            <Col width={[1, 4 / 12]}>
               <Link href="/vender/imovel">
                 <Button height="tall" fluid>
                   Vender meu imóvel
