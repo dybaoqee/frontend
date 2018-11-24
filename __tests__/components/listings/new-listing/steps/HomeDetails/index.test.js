2913ed70ff63f0f62dfa352e8898d2851f15e5ca
Lucas Tulio <lucasctulio@gmail.com>
2018-11-24 10:49:33 -0200

    Add HomeDetails screen tests

---
+++ __tests__/components/listings/new-listing/steps/HomeDetails/index.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeDetails from 'components/listings/new-listing/steps/HomeDetails';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components'
+import theme from '@emcasa/ui'
+import { ThemeProvider } from 'styled-components'
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <ThemeProvider theme={theme}>
+      <HomeDetails />
+    </ThemeProvider>).toJSON()
+  expect(tree).toMatchSnapshot()
+})
