f6e6a09576272cc2c161a5f470be1cef02df28d1
Lucas Tulio <lucasctulio@gmail.com>
2018-11-26 16:22:34 -0200

    Add new tests for Garage step, text and currency functions

---
+++ __tests__/components/listings/new-listing/steps/Garage/index.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Garage from 'components/listings/new-listing/steps/Garage';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components'
+import theme from '@emcasa/ui'
+import { ThemeProvider } from 'styled-components'
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <ThemeProvider theme={theme}>
+      <Garage />
+    </ThemeProvider>).toJSON()
+  expect(tree).toMatchSnapshot()
+})
--- __tests__/utils/text-utils.test.js
+++ __tests__/utils/text-utils.test.js
@@ -1,9 +1,11 @@
 import {
+  getParagraphs,
   currencyToInt,
-  roundUpPrice
+  roundUpPrice,
+  getUrlVars
 } from 'utils/text-utils'
 
-describe('text utils', () => {
+describe('currency formatting', () => {
   it('should transform currency display value into an integer', () =>{
     const displayValue = 'R$ 200.000'
     const intValue = currencyToInt(displayValue)
@@ -14,5 +16,26 @@ describe('text utils', () => {
     const price = 1234567
     const roundedPrice = roundUpPrice(price)
     expect(roundedPrice).toBe(1240000)
+  })
+})
+
+describe('text utils', () => {
+  it('should return an array of paragraphs from a string of text', () => {
+    const text = 'One paragraph.\nTwo paragraphs.\nThree paragraphs.'
+    expect(getParagraphs(text)[0]).toBe('One paragraph.\n')
+    expect(getParagraphs(text)[1]).toBe('Two paragraphs.\n')
+    expect(getParagraphs(text)[2]).toBe('Three paragraphs.')
+  })
+
+  it('should return undefined if no text is passed to getParagraphs', () => {
+    const text = null
+    expect(getParagraphs(text)).toBe(undefined)
+  })
+
+  it('should return url vars', () => {
+    const url = 'https://emcasa.com/test?a=123&b=456'
+    const urlVars = getUrlVars(url)
+    expect(urlVars.a).toBe('123')
+    expect(urlVars.b).toBe('456')
   })
 })
