5c6e452a4ff7ce9cb03bf9ad74e82ce1466c8bc9
Lucas Tulio <lucasctulio@gmail.com>
2018-11-30 22:24:12 -0200

    Update listing flex behavior

--- components/shared/ListingInfiniteScroll/styles.js
+++ components/shared/ListingInfiniteScroll/styles.js
@@ -11,7 +11,7 @@ const ListContainer = styled(Row)`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  justify-content: flex-start;
+  justify-content: center;
 `
 
 const Footer = styled.footer`
