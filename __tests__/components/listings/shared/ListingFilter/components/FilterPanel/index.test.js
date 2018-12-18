c46b81f7dc99faf1159806b82f1a3d8dd4b96f1a
Lucas Tulio <lucasctulio@gmail.com>
2018-12-18 12:23:26 -0200

    Add FilterButton and FilterPanel tests

---
+++ __tests__/components/listings/shared/ListingFilter/components/FilterPanel/index.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import FilterPanel from 'components/listings/shared/ListingFilter/components/FilterPanel'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+import theme from '@emcasa/ui'
+import { ThemeProvider } from 'styled-components'
+
+describe('<FilterPanel/>', () => {
+  it('should render component', () => {
+    const tree = renderer.create(
+      <ThemeProvider theme={theme}>
+        <FilterPanel
+          show={true}
+          apply={() => {}}
+          clear={() => {}}
+          panelPosition={{left: 0, top: 0}}
+        />
+      </ThemeProvider>).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
