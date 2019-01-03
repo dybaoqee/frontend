fae1429649917afb7f4de01e89e8fe4fb4ad817e
Lucas Tulio <lucasctulio@gmail.com>
2019-01-03 15:27:43 -0200

    Apply filter button margin regardless of expand-collapse state

--- components/listings/shared/ListingFilter/styles.js
+++ components/listings/shared/ListingFilter/styles.js
@@ -21,13 +21,13 @@ const expandedWrapper = css`
   flex-wrap: nowrap;
   overflow: scroll hidden;
   height: 40px;
-  margin-right: calc(${EXPAND_BUTTON_WIDTH}px + ${theme.space[4]}px);
 `
 
 const ButtonsWrapper = styled(Row)`
   position: relative;
   flex-direction: row;
   flex-wrap: wrap;
+  margin-right: calc(${EXPAND_BUTTON_WIDTH}px + ${theme.space[4]}px);
 
   ${({expanded}) => expanded ? null : expandedWrapper}
 `
