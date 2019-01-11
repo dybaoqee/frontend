254ad56d5b09d1b3cf565dc7788050f1eba45f9b
Lucas Tulio <lucasctulio@gmail.com>
2019-01-11 15:43:19 -0200

    Display full list of neighborhoods for each city

--- components/shared/NeighborhoodPicker/components/CityContainer/index.js
+++ components/shared/NeighborhoodPicker/components/CityContainer/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Row from '@emcasa/ui-dom/components/Row'
 import Col from '@emcasa/ui-dom/components/Col'
+import View from '@emcasa/ui-dom/components/View'
 import Button from '@emcasa/ui-dom/components/Button'
 import Text from '@emcasa/ui-dom/components/Text'
 import {
@@ -15,13 +16,18 @@ class CityContainer extends Component {
     return (
       <CitiesWrapper>
         {cities.map((city, i) =>
-          <Row key={i} p={2}>
-            <Text fontSize="small">{city.name}</Text>
-            <Row>
-              {city.neighborhoods.map((neighborhood, j) =>
-                <Button key={j}>{neighborhood.name}</Button>
-              )}
-            </Row>
+          <Row key={i} p={2} flexDirection="column">
+            <Col><Text fontSize="small">{city.name}</Text></Col>
+            <Col>
+              <Row flexWrap="wrap">
+                {city.neighborhoods.map((neighborhood, j) =>
+                  <View mr={2} mb={2}>
+                    <Button key={j}>{neighborhood.name}</Button>
+                  </View>
+                )}
+                <Button link>Ver todos</Button>
+              </Row>
+            </Col>
           </Row>
         )}
       </CitiesWrapper>
