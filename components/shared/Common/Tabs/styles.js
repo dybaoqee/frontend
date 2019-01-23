7396a7bfa4d9450a229157e22b3ed1d1f7f34439
Garou <jrgarou@gmail.com>
2019-01-23 16:23:02 -0200

    Update shared Tabs style

--- components/shared/Common/Tabs/styles.js
+++ components/shared/Common/Tabs/styles.js
@@ -4,9 +4,10 @@ import theme from '@emcasa/ui'
 
 export default styled.div`
   box-sizing: border-box;
-  margin: ${({full}) => (full ? '0 auto 20px' : '50px auto 30px;')};
-  padding: ${({full}) => (full ? '40px' : '10px')};
-  width: ${({full}) => (full ? '100%' : '400px')};
+  width: 100%;
+  max-width: ${({full}) => (full ? null : '700px')};
+  margin: ${({full}) => (full ? '0 auto 20px' : `50px auto ${theme.space[5]}px`)};
+  padding: ${({full}) => (full ? '40px' : `${theme.space[2]}px`)};
 
   @media ${mobileMedia} {
     padding: 10px;
@@ -16,7 +17,7 @@ export default styled.div`
 
 export const TabTitles = styled.div`
   display: flex;
-  margin-bottom: 36px;
+  margin-bottom: ${theme.space[5]}px;
 `
 
 export const TabTitle = styled.div`
@@ -29,7 +30,7 @@ export const TabTitle = styled.div`
   letter-spacing: -0.1px;
   line-height: 22px;
   text-align: center;
-  padding: 0px 5px;
+  padding: 0px ${theme.space[1]}px;
   color: ${theme.colors.dark};
   transition: color 0.3s;
   border-bottom: ${({active}) => active ? `1px solid ${theme.colors.pink}` : null};
