36f621686d9a13a1c104fddf3dfe9fb130b81199
Lucas Tulio <lucasctulio@gmail.com>
2019-01-23 05:20:04 -0200

    Remove search bar min width

--- components/shared/NeighborhoodPicker/styles.js
+++ components/shared/NeighborhoodPicker/styles.js
@@ -1,18 +1,12 @@
 import styled from 'styled-components'
-import { themeGet } from 'styled-system'
 import theme from '@emcasa/ui'
-import View from '@emcasa/ui-dom/components/View'
 import Row from '@emcasa/ui-dom/components/Row'
-import Icon from '@emcasa/ui-dom/components/Icon'
 
-const HEIGHT_TALL = theme.buttonHeight[0]
-const HEIGHT_MEDIUM = theme.buttonHeight[1]
 const SEARCH_CONTAINER_WIDTH = 560
 
 const SearchContainer = styled(Row)`
   width: 35%;
   max-width: ${SEARCH_CONTAINER_WIDTH}px;
-  min-width: 250px;
   height: 100%;
   margin: ${theme.space[4]}px 0 0 ${theme.space[4]}px;
 
@@ -32,7 +26,7 @@ const InputContainer = styled(Row)`
   flex-direction: row;
   justify-content: space-between;
   background-color: ${theme.colors.white};
-  height: ${HEIGHT_TALL}px;
+  height: ${theme.buttonHeight[0]}px;
   box-sizing: border-box;
 
   @media (max-width: ${theme.breakpoints[0]}) {
