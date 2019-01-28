844ebbdcfb5574017ebc57feb5e1bfbeaf20d7a1
Lucas Tulio <lucasctulio@gmail.com>
2019-01-28 20:10:18 -0200

    Set correct background color

--- components/shared/NeighborhoodPicker/styles.js
+++ components/shared/NeighborhoodPicker/styles.js
@@ -77,7 +77,7 @@ const ButtonText = styled(Text)`
 const Background = styled(View)`
   position: fixed;
   top: 0; right: 0; bottom: 0; left: 0;
-  background-color: rgba(0, 0, 0, 0.3);
+  background-color: rgba(255, 255, 255, 0.5);
 `
 
 export {
