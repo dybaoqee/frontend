925c760805b90192f136b3e63c2445d94b50e71f
Garou <jrgarou@gmail.com>
2019-01-29 20:26:09 -0200

    Pass user prop to LikeButton

--- pages/user/profile/index.js
+++ pages/user/profile/index.js
@@ -332,6 +332,7 @@ class UserProfile extends Component {
   }
 
   getUserListings = () => {
+    const {user} = this.props
     return (
       <Query query={GET_USER_LISTINGS}>
         {({loading, error, data: {userProfile}}) => {
@@ -349,7 +350,7 @@ class UserProfile extends Component {
                     <ListingCard
                       key={listing.id}
                       listing={listing}
-                      currentUser={userProfile}
+                      currentUser={user}
                       loading={loading}
                       favorited={userProfile.favorites || []}
                     />
@@ -390,6 +391,7 @@ class UserProfile extends Component {
   }
 
   getUserFavorites = () => {
+    const {user} = this.props
     return (
       <Query query={GET_FAVORITE_LISTINGS}>
         {({loading, error, data: {userProfile}}) => {
@@ -408,7 +410,7 @@ class UserProfile extends Component {
                     <ListingCard
                       key={listing.id}
                       listing={listing}
-                      currentUser={userProfile}
+                      currentUser={user}
                       loading={loading}
                       favorited={userProfile.favorites || []}
                     />
