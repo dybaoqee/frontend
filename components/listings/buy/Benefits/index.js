11f60b771b843efbb8c1f49e737e455952a3e363
Garou <jrgarou@gmail.com>
2019-02-05 18:32:35 -0200

    Benefits headings

--- components/listings/buy/Benefits/index.js
+++ components/listings/buy/Benefits/index.js
@@ -9,45 +9,14 @@ import Col from '@emcasa/ui-dom/components/Col'
 import Row from '@emcasa/ui-dom/components/Row'
 import Text from '@emcasa/ui-dom/components/Text'
 import Button from '@emcasa/ui-dom/components/Button'
-
-const Container = styled(View)`
-  display: flex;
-  justify-content: center;
-`
-
-const Content = styled(View)`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  max-width: 1100px;
-  @media (max-width: ${theme.breakpoints[0]}) {
-    padding-left: 20px;
-    padding-right: 20px;
-  }
-`
-
-const Icon = styled.div`
-  background-image: url('/static/svg-icons/${props => props.name}.svg');
-  background-repeat: no-repeat;
-  background-size: cover;
-  width: 70px;
-  height: 70px;
-  margin-bottom: 30px;
-`
-
-const BenefitCol = styled(Col)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  p {
-    margin-bottom: 8px;
-    margin-top: 0;
-  }
-  @media (max-width: ${theme.breakpoints[0]}) {
-    padding-bottom: 50px;
-  }
-`
+import {
+  Container,
+  Content,
+  Title,
+  SubTitle,
+  Icon,
+  BenefitCol
+} from './styles'
 
 const BENEFITS = [
   {
@@ -73,9 +42,9 @@ const getBenefits = (isMobile) => {
   const benefits = BENEFITS.map(({icon, title, description}) => (
     <BenefitCol width={[1, 3 / 12]}>
       <Icon name={icon} />
-      <Text fontSize="large" color="dark">
+      <SubTitle fontSize="large" fontWeight="normal" color="dark">
         {title}
-      </Text>
+      </SubTitle>
       <Text color="gray">{description}</Text>
     </BenefitCol>
   ))
@@ -103,9 +72,9 @@ export default class Benefits extends Component {
         <Content>
           <Row justifyContent="center">
             <Col>
-              <Text fontSize="large" color="dark" fontWeight="bold" textAlign="center">
+              <Title fontSize="large" color="dark" fontWeight="bold" textAlign="center">
                 Conheça as vantagens de comprar com a EmCasa
-              </Text>
+              </Title>
             </Col>
           </Row>
           <Row
