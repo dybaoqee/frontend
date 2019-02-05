3f1f5f3adf3d7326c379f60d5935ded0b8ce1543
Lucas Tulio <lucasctulio@gmail.com>
2019-02-04 22:06:21 -0200

    Animate filter buttons expand-collapse action on mobile

--- components/listings/shared/ListingFilter/index.js
+++ components/listings/shared/ListingFilter/index.js
@@ -217,20 +217,23 @@ class ListingFilter extends Component {
         price: userPrice,
         rooms: userRooms,
         garageSpots: userGarageSpots
-      }
+      },
+      expanded
     } = this.state
 
     const isFilterOpen = this.isFilterOpen()
 
     return (
       <Container isFilterOpen={isFilterOpen}>
-        <ButtonsWrapper expanded={this.state.expanded}>
-          {getFilterButtons(this.props.filters, this.showFilter, this.getOpenButton)}
-          <ExpandButton
-            expanded={this.state.expanded}
-            onClick={this.toggleFilters}
-          />
-        </ButtonsWrapper>
+        <PoseGroup>
+            <ButtonsWrapper expanded={expanded} key={1} pose={expanded ? 'open' : 'closed'}>
+              {getFilterButtons(this.props.filters, this.showFilter, this.getOpenButton)}
+              <ExpandButton
+                expanded={expanded}
+                onClick={this.toggleFilters}
+              />
+            </ButtonsWrapper>
+        </PoseGroup>
         <FilterPanel
           filter={FILTERS.TYPES}
           show={this.state.showType}
