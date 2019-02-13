a68910b59162667a51ccec9dee3fe0e248dfd4af
Lucas Tulio <lucasctulio@gmail.com>
2019-02-13 11:18:35 -0200

    Restore seller home

---
+++ pages/listings/sell/know-more.js
@@ -0,0 +1,125 @@
+import {Component, Fragment} from 'react'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faGlobe from '@fortawesome/fontawesome-pro-light/faGlobe'
+import faGift from '@fortawesome/fontawesome-pro-light/faGift'
+import faBolt from '@fortawesome/fontawesome-pro-light/faBolt'
+import faCoin from '@fortawesome/fontawesome-pro-light/faUsdCircle'
+import faPaste from '@fortawesome/fontawesome-pro-light/faPaste'
+import faGavel from '@fortawesome/fontawesome-pro-light/faGavel'
+import Link from 'next/link'
+import Head from 'next/head'
+import {imageUrl} from 'utils/image_url'
+import EmCasaButton from 'components/shared/Common/Buttons'
+import Topics from 'components/shared/Common/Topics'
+import Container, {Header, BenefitsContainer, Benefit} from './styles'
+
+import PriceEstimate from 'components/shared/PriceEstimate'
+
+export default class SellKnowMore extends Component {
+  render() {
+    const seoImg = imageUrl(
+      'emcasa-saiba-mais-para-vender-share-centered-2.jpg'
+    )
+    const seoTitle = 'Anuncie e Venda seu imóvel no Rio de Janeiro | EmCasa'
+    const seoDescription =
+      'Anuncie e venda seu imóvel no Rio de Janeiro de forma simples e transparente com a EmCasa que tem sistema exclusivo de Tour Virtual 3D para aumentar suas chances de venda'
+
+    return (
+      <Fragment>
+        <Head>
+          <title>{seoTitle}</title>
+          <meta name="description" content={seoDescription} />
+          <meta property="og:description" content={seoDescription} />
+          <meta property="og:image" content={seoImg} />
+          <meta property="og:image:height" content="838" />
+          <meta property="og:image:width" content="1476" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={seoTitle} />
+          <meta name="twitter:description" content={seoDescription} />
+          <meta name="twitter:image" content={seoImg} />
+        </Head>
+        <Fragment>
+          <Header>
+            <img
+              src={imageUrl('listings_new_header.png')}
+              alt="Venda seu imóvel na EmCasa"
+            />
+            <h1>Anuncie e Venda seu imóvel no Rio de Janeiro com a EmCasa</h1>
+          </Header>
+          <Container>
+            <BenefitsContainer>
+              <Benefit>
+                <FontAwesomeIcon icon={faGlobe} />
+                <p>Tenha acesso imediato a milhares de compradores.</p>
+              </Benefit>
+              <Benefit>
+                <FontAwesomeIcon icon={faGift} />
+                <p>
+                  Ganhe um Tour Virtual em 3D e atraia mais atenção para o seu
+                  imóvel.
+                </p>
+              </Benefit>
+              <Benefit>
+                <FontAwesomeIcon icon={faBolt} />
+                <p>Evite visitas desnecessárias em sua casa.</p>
+              </Benefit>
+              <Benefit>
+                <FontAwesomeIcon icon={faCoin} />
+                <p>Assistência jurídica grátis com documentação e processos.</p>
+              </Benefit>
+              <Benefit>
+                <FontAwesomeIcon icon={faPaste} />
+                <p>Suporte em financiamento e retirada de FGTS.</p>
+              </Benefit>
+              <Benefit>
+                <FontAwesomeIcon icon={faGavel} />
+                <p>Economize tempo e venda seu imóvel mais rápido.</p>
+              </Benefit>
+            </BenefitsContainer>
+            <Link passHref href="/listings/new" as="/imoveis/adicionar">
+              <a>
+                <EmCasaButton light>Anuncie agora</EmCasaButton>
+              </a>
+            </Link>
+          </Container>
+          <Container id="precificador">
+            <PriceEstimate />
+          </Container>
+          <Container>
+            <h3>Cadastre seu imóvel em menos de 5 minutos</h3>
+            <Topics showNumbers>
+              <div>
+                <h4>
+                  Cadastre-se na plataforma e preencha as informações básicas do
+                  seu imóvel
+                </h4>
+              </div>
+              <div>
+                <h4>
+                  Envie as fotos do imóvel e agende o nosso Tour Virtual 3D
+                </h4>
+              </div>
+              <div>
+                <h4>
+                  Envie os documentos e receba uma avaliação gratuita do seu
+                  imóvel
+                </h4>
+              </div>
+            </Topics>
+            <p className="warning">
+              <strong>Pronto!</strong> Seu imóvel estará no ar e nossa equipe
+              iniciará o processo de anúncio e venda de seu apartamento ou casa.
+              Agora é aguardar nosso contato para agendamento das visitas com os
+              interessados em comprar o seu imóvel.
+            </p>
+            <Link passHref href="/listings/new" as="/imoveis/adicionar">
+              <a>
+                <EmCasaButton light>Anuncie agora</EmCasaButton>
+              </a>
+            </Link>
+          </Container>
+        </Fragment>
+      </Fragment>
+    )
+  }
+}
---
+++ pages/listings/sell/styles.js
@@ -0,0 +1,217 @@
+import styled from 'styled-components'
+
+import {headerMobileMedia, mobileMedia} from 'constants/media'
+import * as colors from 'constants/colors'
+
+export default styled.div`
+  padding: 50px 0 100px;
+  display: flex;
+  flex-direction: column;
+  border-top: 1px solid ${colors.lightestGray};
+  font-family: 'Open Sans';
+
+  * {
+    box-sizing: border-box;
+  }
+  h2 {
+    text-transform: uppercase;
+    height: 17px;
+    width: 100%;
+    font-size: 12px;
+    font-weight: 700;
+    letter-spacing: 0.4px;
+    line-height: 17px;
+    text-align: center;
+    padding: 10px;
+    margin-bottom: 60px;
+  }
+
+  h3 {
+    width: 100%;
+    font-size: 32px;
+    font-weight: 300;
+    letter-spacing: -0.3px;
+    line-height: 48px;
+    text-align: center;
+    margin: auto;
+    padding: 0 40px;
+    margin-bottom: 20px;
+  }
+
+  > button {
+    box-sizing: border-box;
+    width: 384px;
+    margin: auto;
+    @media ${mobileMedia} {
+      max-width: calc(100% - 40px);
+    }
+  }
+
+  svg {
+    width: 30px !important;
+    height: 30px;
+
+    transform: rotate(0.001deg);
+  }
+
+  p.warning {
+    width: 588px;
+    color: ${colors.green.medium};
+    font-size: 16px;
+    line-height: 28px;
+    text-align: center;
+    margin: 0 auto 81px;
+    padding: 0 40px;
+
+    @media ${headerMobileMedia} {
+      width: 100%;
+    }
+  }
+`
+
+export const Header = styled.div`
+  height: 102px;
+  position: relative;
+  * {
+    box-sizing: border-box;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  h1 {
+    position: absolute;
+    top: -2px;
+    z-index: 1;
+    color: white;
+    font-weight: 300;
+    font-size: 36px;
+    letter-spacing: -0.4px;
+    line-height: 1.4em;
+    text-shadow: 0 2px 16px 0 rgba(0, 0, 0, 0.4);
+    width: 100%;
+    text-align: center;
+    padding: 20px;
+    margin: 0;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    @media ${mobileMedia} {
+      font-size: 24px;
+    }
+  }
+`
+
+export const BenefitsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 240px 240px 240px;
+  justify-content: center;
+  grid-gap: 30px;
+  margin: 0 120px 50px;
+
+  @media ${headerMobileMedia} {
+    margin: 0 40px 90px;
+    grid-gap: 80px;
+    grid-template-columns: 240px 240px;
+  }
+  @media ${mobileMedia} {
+    margin: 0 40px 90px;
+    grid-template-columns: calc(100% - 40px);
+    grid-gap: 40px;
+  }
+`
+export const Benefit = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  [data-icon='globe'] {
+    path {
+      color: ${colors.blue.medium};
+    }
+  }
+  [data-icon='gift'] {
+    path {
+      color: ${colors.red.medium};
+    }
+  }
+  [data-icon='bolt'] {
+    path {
+      color: ${colors.orange.medium};
+    }
+  }
+  [data-icon='usd-circle'] {
+    path {
+      color: ${colors.green.medium};
+    }
+  }
+  [data-icon='paste'] {
+    path {
+      color: ${colors.mediumGray};
+    }
+  }
+  [data-icon='gavel'] {
+    path {
+      color: ${colors.text};
+    }
+  }
+  div {
+    width: 78px;
+    height: 78px;
+    border-radius: 100%;
+    background: ${colors.blue.light};
+    margin: auto;
+  }
+
+  p {
+    width: 240px;
+    font-size: 16px;
+    line-height: 30px;
+    text-align: center;
+    color: ${colors.gray4a};
+  }
+`
+
+export const CardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+export const Card = styled.div`
+  height: 480px;
+  width: 432px;
+  border-radius: 12px;
+  box-shadow: inset 0 2px 2px 0 #ffffff, 0 8px 20px 0 rgba(38, 38, 38, 0.15);
+  overflow: hidden;
+  margin: 0 24px 24px 24px;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 50% 50%;
+
+  div:first-of-type {
+    background-color: ${colors.blue.light};
+  }
+
+  h5 {
+    width: 100%;
+    font-size: 26px;
+    font-weight: 300;
+    line-height: 36px;
+    text-align: center;
+    margin: 30px 0;
+    padding: 0 40px;
+  }
+
+  p {
+    width: 100%;
+    color: ${colors.mediumDarkGray};
+    font-size: 16px;
+    line-height: 28px;
+    text-align: center;
+    padding: 0 40px;
+  }
+`
