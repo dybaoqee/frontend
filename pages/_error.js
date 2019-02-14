86221d33292b3d869d5e21d8ea294ac868d93a7a
Garou <jrgarou@gmail.com>
2019-02-14 17:08:59 -0200

    HTTPMessage component

--- pages/_error.js
+++ pages/_error.js
@@ -1,7 +1,5 @@
-import {Component, Fragment} from 'react'
-import Error from 'components/shared/Shell/Error'
-import Link from 'next/link'
-import codes from 'constants/statusCodes'
+import {Component} from 'react'
+import HTTPMessage from 'components/shared/Shell/HTTPMessage'
 
 export default class ErrorPage extends Component {
   static getInitialProps({err, res}) {
@@ -15,38 +13,11 @@ export default class ErrorPage extends Component {
     }
   }
 
-  get title() {
-    const {statusCode} = this.props
-
-    return codes[statusCode]
-  }
-
-  get message() {
-    const {statusCode} = this.props
-
-    switch (statusCode) {
-      case 404:
-        return 'O link que você clicou pode estar quebrado ou a página pode ter sido removida.'
-      default:
-        return null
-    }
-  }
-
   render() {
     const {statusCode} = this.props
 
     return (
-      <Fragment>
-        <Error>
-          <h1>{this.title}</h1>
-          <h2>{statusCode}</h2>
-          <p>{this.message}</p>
-          <p>
-            Visite nossa <Link href="/">página inicial</Link> ou entre em&nbsp;
-            <Link href="mailto:contato@emcasa.com">contato</Link> com a gente
-          </p>
-        </Error>
-      </Fragment>
+      <HTTPMessage statusCode={statusCode} />
     )
   }
 }
