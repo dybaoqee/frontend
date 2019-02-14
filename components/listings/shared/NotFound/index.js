d14859cad2b67af3f7ee60cb6c631ed57ffe3746
Garou <jrgarou@gmail.com>
2019-02-14 21:37:32 -0200

    Not found list items

--- components/listings/shared/NotFound/index.js
+++ components/listings/shared/NotFound/index.js
@@ -6,6 +6,12 @@ import {
   log,
   LISTING_SEARCH_NOT_FOUND
 } from 'lib/logging'
+import Text from '@emcasa/ui-dom/components/Text'
+import Row from '@emcasa/ui-dom/components/Row'
+import Col from '@emcasa/ui-dom/components/Col'
+import Button from '@emcasa/ui-dom/components/Button'
+import {Query} from 'react-apollo'
+import {GET_DISTRICTS} from 'graphql/listings/queries'
 
 class NotFound extends PureComponent {
   componentDidMount() {
@@ -13,43 +19,75 @@ class NotFound extends PureComponent {
   }
 
   render() {
-    const {
-      resetAllParams,
-      filtered,
-      messages,
-      href = '/listings',
-      as = '/imoveis'
-    } = this.props
-
-    if (filtered) {
-      return (
-        <Container onClick={resetAllParams}>
-          <div>
-            <p>Não encontramos imóveis com base nesses critérios, tente alterar os filtros para outros resultados</p>
-          </div>
-        </Container>
-      )
-    }
+    const {filters, params} = this.props
 
     return (
-      <Link href={href} as={as}>
-        <Container>
-          <div>
-            {messages &&
-              messages.map((message) => <p key={message}>{message}</p>)}
-          </div>
-        </Container>
-      </Link>
+      <Col px={4}>
+        <Text fontWeight="bold">Nenhum imóvel encontrado</Text>
+        <Text color="grey" my={0}>Tente alterar os critérios selecionados para encontrar outros imóveis.</Text>
+        <Query query={GET_DISTRICTS}>
+          {({loading, error, data}) => {
+            const districts = data.districts
+
+            if (loading) return (<div/>)
+            if (error) return `Error! ${error.message}`
+
+            const findState = districts.find(a => a.stateSlug === params.state)
+            const findCity = districts.find(a => a.citySlug === params.city)
+
+            if (findCity) {
+              return (
+                <Col>
+                  <Link passHref href={`/imoveis/${findCity.stateSlug}/${findCity.citySlug}`}>
+                    <a>
+                      <Button
+                        height="tall"
+                      >
+                        Explorar imóveis em {`${findCity.city}`}
+                      </Button>
+                    </a>
+                  </Link>
+                </Col>
+              )
+            } else if (findState) {
+              return (
+                <Col>
+                  <Link passHref href={`/imoveis/${findState.stateSlug}`}>
+                    <a>
+                      <Button
+                        height="tall"
+                      >
+                        Explorar imóveis em {`${findState.state}`}
+                      </Button>
+                    </a>
+                  </Link>
+                </Col>
+              )
+            } else {
+              return (
+                <Col>
+                  <Link passHref href="/imoveis">
+                    <a>
+                      <Button
+                        height="tall"
+                      >
+                        Explorar imóveis
+                      </Button>
+                    </a>
+                  </Link>
+                </Col>
+              )
+            }
+          }}
+        </Query>
+      </Col>
     )
   }
 }
 
 NotFound.propTypes = {
-  resetAllParams: PropTypes.func.isRequired,
-  filtered: PropTypes.bool.isRequired,
-  messages: PropTypes.string.isRequired,
-  href: PropTypes.string,
-  as: PropTypes.string
+  filters: PropTypes.object.isRequired,
+  params: PropTypes.object.isRequired
 }
 
 export default NotFound
