5c7d61ec5ff047deb0df73f92707039577d651be
Garou <jrgarou@gmail.com>
2019-02-18 13:14:19 -0300

    Just show a text to NotFound search

--- components/listings/shared/NotFound/index.js
+++ components/listings/shared/NotFound/index.js
@@ -1,17 +1,11 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import Link from 'next/link'
-import Container from './styles'
 import {
   log,
   LISTING_SEARCH_NOT_FOUND
 } from 'lib/logging'
-import Text from '@emcasa/ui-dom/components/Text'
-import Row from '@emcasa/ui-dom/components/Row'
 import Col from '@emcasa/ui-dom/components/Col'
-import Button from '@emcasa/ui-dom/components/Button'
-import {Query} from 'react-apollo'
-import {GET_DISTRICTS} from 'graphql/listings/queries'
+import Text from '@emcasa/ui-dom/components/Text'
 
 class NotFound extends PureComponent {
   componentDidMount() {
@@ -19,49 +13,11 @@ class NotFound extends PureComponent {
   }
 
   render() {
-    const {filters, params} = this.props
-
     return (
-      <Query query={GET_DISTRICTS}>
-        {({loading, error, data}) => {
-          if (loading) return (<div/>)
-          if (error) return `Error! ${error.message}`
-
-          const districts = data.districts
-          const findState = districts.find(a => a.stateSlug === params.state)
-          const findCity = districts.find(a => a.citySlug === params.city)
-          let linkHref = undefined
-          let linkLabel = undefined
-
-          if (findCity) {
-            linkHref = `/imoveis/${findCity.stateSlug}/${findCity.citySlug}`
-            linkLabel = `Explorar imóveis em ${findCity.city}`
-          } else if (findState) {
-            linkHref = `/imoveis/${findState.stateSlug}`
-            linkLabel = `Explorar imóveis em ${findState.state}`
-          }
-
-          return (
-            <Col px={4}>
-              <Text fontWeight="bold">Nenhum imóvel encontrado</Text>
-              <Text color="grey" my={0}>Tente alterar os critérios selecionados para encontrar outros imóveis.</Text>
-              {linkHref && linkLabel && (
-                <Col>
-                  <Link passHref href={linkHref}>
-                    <a>
-                      <Button
-                        height="tall"
-                      >
-                        {linkLabel}
-                      </Button>
-                    </a>
-                  </Link>
-                </Col>
-              )}
-            </Col>
-          )
-        }}
-      </Query>
+      <Col px={4}>
+        <Text fontWeight="bold">Nenhum imóvel encontrado</Text>
+        <Text color="grey" my={0}>Tente alterar os critérios selecionados para encontrar outros imóveis.</Text>
+      </Col>
     )
   }
 }
