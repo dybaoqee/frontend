99732464a6e9d710ae46b70f5b7b4a0a60f93a19
Lucas Tulio <lucasctulio@gmail.com>
2019-02-27 14:38:13 -0300

    Fade animation in price display transition

--- components/listings/new-listing/steps/Pricing/index.js
+++ components/listings/new-listing/steps/Pricing/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Formik, Field } from 'formik'
+import { PoseGroup } from 'react-pose'
 import MaskedInput from 'react-text-mask'
 import Icon from '@emcasa/ui-dom/components/Icon'
 import Input from '@emcasa/ui-dom/components/Input'
@@ -9,6 +10,8 @@ import Col from '@emcasa/ui-dom/components/Col'
 import Text from '@emcasa/ui-dom/components/Text'
 import NavButtons from 'components/listings/new-listing/shared/NavButtons'
 import {autoFocus} from 'components/listings/new-listing/lib/forms'
+import FadeInOut from 'components/shared/Animation/FadeInOut'
+import { randomKey } from 'lib/random'
 import {
   currencyInputMask,
   currencyToInt,
@@ -224,68 +227,72 @@ class Pricing extends Component {
                     textAlign="center">
                     Qual o valor do seu imóvel?
                   </Text>
-                  {this.state.showPrice ?
-                  <>
-                    <Row alignItems="center" flexDirection="column">
-                      {suggestedPrice && <Text color="grey">Nossa avaliação é precisa de acordo com os valores de mercado da sua região. Você mesmo pode editar este valor ou conversar com um de nossos especialistas no final do processo.</Text>}
-                      <Ticket
-                        hideSeparator={!suggestedPrice}
-                        topRender={() =>
-                          <Row px={4} pt={4} pb={suggestedPrice ? 4 : 0} flexDirection="column">
-                            <Text inline fontSize="small" fontWeight="bold">{location.address}</Text>
-                            {suggestedPrice ? <Row mt={2}><Text inline fontSize="xsmall" color="grey">{this.getListingSummary()}</Text></Row> : null}
-                          </Row>
-                        }
-                        bottomRender={() =>
-                          <Row px={4} pb={4} pt={suggestedPrice ? 4 : 0} flexDirection="column">
-                            {suggestedPrice ?
-                              <>
-                                <Text inline fontSize="xsmall" color="grey">VALOR AVALIADO</Text>
-                                <Row justifyContent="space-between">
-                                  {this.state.editingPrice ?
-                                    <>
-                                      {this.currencyInput(errors, setFieldValue, setFieldTouched)}
-                                    </>
-                                  :
-                                    <>
-                                      <Text inline fontSize="large" fontWeight="bold">{this.state.userPrice ? intToCurrency(this.state.userPrice) : intToCurrency(roundUpPrice(suggestedPrice))}</Text>
-                                      <Col onClick={() => {
-                                        if (!this.state.editingPrice) {
-                                          log(SELLER_ONBOARDING_EDIT_PRICE)
-                                        }
-                                        this.setState({editingPrice: true})
-                                      }} style={{cursor: 'pointer', marginTop: 8}}>
-                                        <Icon name="pen" />
-                                      </Col>
-                                    </>
-                                  }
-                                </Row>
-                              </>
-                            :
-                              <Row><Text inline fontSize="small" color="grey">{this.getListingSummary()}</Text></Row>
-                            }
-                          </Row>
-                        }
+                  <PoseGroup>
+                    {this.state.showPrice ?
+                    <FadeInOut key={randomKey()}>
+                      <Row alignItems="center" flexDirection="column">
+                        {suggestedPrice && <Text color="grey">Nossa avaliação é precisa de acordo com os valores de mercado da sua região. Você mesmo pode editar este valor ou conversar com um de nossos especialistas no final do processo.</Text>}
+                        <Ticket
+                          hideSeparator={!suggestedPrice}
+                          topRender={() =>
+                            <Row px={4} pt={4} pb={suggestedPrice ? 4 : 0} flexDirection="column">
+                              <Text inline fontSize="small" fontWeight="bold">{location.address}</Text>
+                              {suggestedPrice ? <Row mt={2}><Text inline fontSize="xsmall" color="grey">{this.getListingSummary()}</Text></Row> : null}
+                            </Row>
+                          }
+                          bottomRender={() =>
+                            <Row px={4} pb={4} pt={suggestedPrice ? 4 : 0} flexDirection="column">
+                              {suggestedPrice ?
+                                <>
+                                  <Text inline fontSize="xsmall" color="grey">VALOR AVALIADO</Text>
+                                  <Row justifyContent="space-between">
+                                    {this.state.editingPrice ?
+                                      <>
+                                        {this.currencyInput(errors, setFieldValue, setFieldTouched)}
+                                      </>
+                                    :
+                                      <>
+                                        <Text inline fontSize="large" fontWeight="bold">{this.state.userPrice ? intToCurrency(this.state.userPrice) : intToCurrency(roundUpPrice(suggestedPrice))}</Text>
+                                        <Col onClick={() => {
+                                          if (!this.state.editingPrice) {
+                                            log(SELLER_ONBOARDING_EDIT_PRICE)
+                                          }
+                                          this.setState({editingPrice: true})
+                                        }} style={{cursor: 'pointer', marginTop: 8}}>
+                                          <Icon name="pen" />
+                                        </Col>
+                                      </>
+                                    }
+                                  </Row>
+                                </>
+                              :
+                                <Row><Text inline fontSize="small" color="grey">{this.getListingSummary()}</Text></Row>
+                              }
+                            </Row>
+                          }
+                        />
+                      </Row>
+                      {!suggestedPrice && this.noPriceSuggestion(errors, setFieldValue, setFieldTouched)}
+                      <NavButtons
+                        nextLabel={showEditingPriceLabels ? 'OK' : 'Avançar'}
+                        previousLabel={showEditingPriceLabels ? 'Cancelar' : 'Voltar'}
+                        previousStep={this.previousStep}
+                        onSubmit={this.nextStep}
+                        submitEnabled={isValid}
                       />
-                    </Row>
-                    {!suggestedPrice && this.noPriceSuggestion(errors, setFieldValue, setFieldTouched)}
-                    <NavButtons
-                      nextLabel={showEditingPriceLabels ? 'OK' : 'Avançar'}
-                      previousLabel={showEditingPriceLabels ? 'Cancelar' : 'Voltar'}
-                      previousStep={this.previousStep}
-                      onSubmit={this.nextStep}
-                      submitEnabled={isValid}
-                    />
-                  </>
-                  :
-                    <LoadingText
-                      sentences={[
-                        'Buscando imóveis do mesmo padrão na região',
-                        'Avaliando média do m²',
-                        'Comparando valores de imóveis na mesma rua'
-                      ]}
-                    />
-                  }
+                    </FadeInOut>
+                    :
+                    <FadeInOut key={randomKey()}>
+                      <LoadingText
+                        sentences={[
+                          'Buscando imóveis do mesmo padrão na região',
+                          'Avaliando média do m²',
+                          'Comparando valores de imóveis na mesma rua'
+                        ]}
+                      />
+                      </FadeInOut>
+                    }
+                  </PoseGroup>
                 </>
               )}
             />
