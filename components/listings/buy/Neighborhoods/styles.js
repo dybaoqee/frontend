942aa5307e625125ecf27e17c9f0ea15b487e906
Garou <jrgarou@gmail.com>
2019-03-08 15:15:23 -0300

    Ajust Neighbourhoods style home

--- components/listings/buy/Neighborhoods/styles.js
+++ components/listings/buy/Neighborhoods/styles.js
@@ -4,131 +4,73 @@ import {breakpoint} from '@emcasa/ui/lib/styles'
 import View from '@emcasa/ui-dom/components/View'
 import Text from '@emcasa/ui-dom/components/Text'
 
-export const Container = styled(View)`
-  display: flex;
-  width: 100%;
-`
-
-export const Content = styled(View)`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-`
+const thumbWidth = 345
 
 export const Title = Text.withComponent('h2')
 export const SubTitle = Text.withComponent('h3')
-export const ListTitle = Text.withComponent('h4')
+
+export const Container = styled(View)`
+  width: 100%;
+  max-width: ${1204 + theme.space[4]}px;
+  display: flex;
+  flex-direction: column;
+  padding: 0 ${theme.space[4]}px;
+`
 
 export const Header = styled(View)`
+  margin-bottom: ${theme.space[4]}px;
+
   ${Title} {
-    margin-bottom: 0;
+    margin: 0;
   }
 
-  @media ${breakpoint.down('tablet')} {
-    padding: 20px;
+  p {
+    margin-top: 0;
   }
 `
 
 export const Cities = styled(View)`
-  width: 100%;
   display: flex;
   flex-direction: column;
-  align-items: center;
-
-  @media ${breakpoint.down('tablet')} {
-    align-items: unset;
-  }
 `
 
 export const City = styled(View)`
-  @media ${breakpoint.down('tablet')} {
-    margin-left: 20px;
-  }
-`
-
-export const CityTitle = styled(View)``
-
-export const CityInfo = styled(View)`
-  @media ${breakpoint.down('tablet')} {
-    display: none;
-  }
-`
-
-export const NeighborhoodContainer = styled(View)`
-  position: relative;
-
-  @media ${breakpoint.down('tablet')} {
-    &:after {
-      background: linear-gradient(to left, white, rgba(255, 255, 255, 0));
-      content: ' ';
-      position: absolute;
-      top: 0;
-      right: 0;
-      width: 20%;
-      height: 100%;
-      pointer-events: none;
-    }
-  }
-`
-
-export const NeighborhoodsLinks = styled(View)`
   display: flex;
-  flex-wrap: wrap;
-  max-width: 1000px;
-  max-height: 130px;
-  margin-top: ${theme.space[3]}px;
-  margin-bottom: ${theme.space[3]}px;
   flex-direction: column;
+  margin-bottom: ${theme.space[2]}px;
 
-  .NeighborhoodLink {
-    font-family: FaktSoftPro-Normal;
-    color: ${theme.colors.grey};
-    font-size: 14px;
-    text-decoration: none;
-    width: 345px;
-    margin: 0 ${theme.space[3]}px 0 0;
-
-    ${ListTitle} {
-      margin: 0;
-      font-size: inherit;
-      color: inherit;
-    }
-  }
-
-  @media ${breakpoint.down('tablet')} {
-    max-height: 70px;
-    .NeighborhoodLink {
-      width: 130px;
-      font-size: 8px;
-      margin: 0 ${theme.space[2]}px 0 0;
-    }
+  ${SubTitle} {
+    margin-bottom: 0;
   }
 `
 
-export const NeighborhoodItems = styled(View)`
-  display: flex;
-  flex-wrap: nowrap;
-  position: relative;
+export const NeighborhoodsHighlights = styled(View)`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 0 10px;
+  margin: ${theme.space[3]}px 0 0;
+
   @media ${breakpoint.down('tablet')} {
-    overflow-y: hidden;
-    overflow-x: auto;
-    padding-bottom: ${theme.space[3]}px;
+    grid-template-columns: 1fr;
   }
 `
 
-export const Neighborhood = styled(View)`
+export const Neighborhood = styled('a')`
   position: relative;
   display: flex;
-  flex: 0 0 auto;
   align-items: center;
   justify-content: center;
-  width: 345px;
+  width: 100%;
   height: 185px;
   max-width: 100%;
-  cursor: pointer;
   border-radius: 4px;
   box-shadow: 1px 2px 4px 0 rgba(0, 0, 0, 0.3);
-  margin-right: ${theme.space[2]}px;
+
+  @media ${breakpoint.down('tablet')} {
+    flex: 1 1 100%;
+    height: 125px;
+    margin-bottom: ${theme.space[2]}px;
+  }
 
   p {
     color: white;
@@ -144,17 +86,6 @@ export const Neighborhood = styled(View)`
     height: 100%;
     z-index: -1;
   }
-
-  @media ${breakpoint.down('tablet')} {
-    width: auto;
-    min-width: 130px;
-    min-height: 70px;
-    height: auto;
-
-    p {
-      font-size: 100%;
-    }
-  }
 `
 
 export const Soon = styled(View)`
@@ -165,38 +96,40 @@ export const Soon = styled(View)`
   align-items: center;
   justify-content: center;
   background: ${theme.colors.pink};
-  width: 95px;
-  height: 32px;
+  padding: ${theme.space[1]}px ${theme.space[2]}px;
   border-radius: 4px;
-
-  font-family: 'FaktSoftPro-Normal';
   color: white;
-  font-size: 90%;
+  font-size: ${theme.fontSizes[1]}px;
+`
 
-  &:after {
-    content: 'Em breve';
-  }
+export const NeighborhoodsLinks = styled(View)`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 0 10px;
+  margin: ${theme.space[3]}px 0;
 
   @media ${breakpoint.down('tablet')} {
-    top: 5px;
-    right: 5px;
+    grid-template-columns: repeat(2, 1fr);
+  }
 
-    max-width: 46px;
-    max-height: 17px;
-    font-size: 8px;
+  .NeighborhoodLink {
+    font-size: ${theme.fontSizes[2]}px;
+    padding: ${theme.space[1]}px 0;
+
+    &:hover {
+      color: ${theme.colors.pink};
+    }
+
+    @media ${breakpoint.down('tablet')} {
+      padding: ${theme.space[2]}px 0;
+    }
   }
 `
 
-export const Spacer = styled(View)`
-  display: none;
-  @media ${breakpoint.down('tablet')} {
-    display: flex;
-    max-height: 70px;
-    &:after {
-      display: flex;
-      width: 20px;
-      height: 30px;
-      content: ' ';
-    }
-  }
+const NeighborhoodsLinkTitle = Text.withComponent('h4')
+
+export const ListTitle = styled(NeighborhoodsLinkTitle)`
+  font-size: inherit;
+  margin: 0;
+  color: inherit;
 `
