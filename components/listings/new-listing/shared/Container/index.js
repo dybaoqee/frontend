ac9116da83e60fd3ef99774bd2e4a3130bc9748e
Lucas Tulio <lucasctulio@gmail.com>
2019-03-08 19:12:46 -0300

    Fix step width calc

--- components/listings/new-listing/shared/Container/index.js
+++ components/listings/new-listing/shared/Container/index.js
@@ -6,7 +6,7 @@ import Row from '@emcasa/ui-dom/components/Row'
 const PADDING = `${theme.space[4]}px`
 
 const Container = styled(Row)`
-  width: calc(100vw - ${PADDING * 2}px);
+  width: calc(100vw - 40px);
   padding: 80px ${PADDING} ${PADDING} ${PADDING};
   justify-content: center;
 `
