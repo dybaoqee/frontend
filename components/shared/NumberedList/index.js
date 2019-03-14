388520819b356ef916e6020c8a0c9daae2574cc2
Lucas Tulio <lucasctulio@gmail.com>
2019-03-14 18:51:45 -0300

    Create NumberedList component

---
+++ components/shared/NumberedList/index.js
@@ -0,0 +1,33 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import Text from '@emcasa/ui-dom/components/Text'
+import {
+  Container,
+  Item,
+  Number
+} from './styles'
+
+class NumberedList extends PureComponent {
+  render() {
+    const { content } = this.props
+    return (
+      <Container>
+        {content.map((item, index) => {
+          const last = index === content.length - 1
+          return (
+            <Item key={index}>
+              <Number color="white" last={last}>{index + 1}</Number>
+              <Text inline>{item}</Text>
+            </Item>
+          )
+        })}
+      </Container>
+    )
+  }
+}
+
+NumberedList.propTypes = {
+  content: PropTypes.array
+}
+
+export default NumberedList
