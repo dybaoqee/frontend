aeae524e2879abc6338d5908309f398fb05c61f6
Lucas Tulio <lucasctulio@gmail.com>
2019-03-21 11:18:33 -0300

    Parsing old format phone numbers with ic and no plus sign

--- __tests__/lib/user/index.test.js
+++ __tests__/lib/user/index.test.js
@@ -10,6 +10,14 @@ describe('user functions', () => {
   })
 
   it('should return parts of the phone number, when it has international code', () => {
+    const phone = '5511987654321'
+    const phoneObject = getPhoneParts(phone)
+    const { localAreaCode, number } = phoneObject
+    expect(localAreaCode).toBe('11')
+    expect(number).toBe('987654321')
+  })
+
+  it('should return parts of the phone number, when it has international code with plus sign', () => {
     const phone = '+5511987654321'
     const phoneObject = getPhoneParts(phone)
     const { localAreaCode, number } = phoneObject
