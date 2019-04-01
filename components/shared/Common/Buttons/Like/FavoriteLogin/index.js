0b203f6800abae13c2ce3ec693d0a4f7702e2e05
Lucas Tulio <lucasctulio@gmail.com>
2019-04-01 17:32:48 -0300

    Favorite login background with better handling of click outside

--- components/shared/Common/Buttons/Like/FavoriteLogin/index.js
+++ components/shared/Common/Buttons/Like/FavoriteLogin/index.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import enhanceWithClickOutside from 'react-click-outside'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faHeart from '@fortawesome/fontawesome-free-solid/faHeart'
+import View from '@emcasa/ui-dom/components/View'
+import Row from '@emcasa/ui-dom/components/Row'
+import Col from '@emcasa/ui-dom/components/Col'
+import Text from '@emcasa/ui-dom/components/Text'
+import Input from '@emcasa/ui-dom/components/Input'
 import {
-  Container
+  Background,
+  Container,
+  HeartContainer
 } from './styles'
 
-class FavoriteLogin extends Component {
+class ContainerClickOutside extends Container {
   handleClickOutside(e) {
     e.preventDefault()
     this.props.onClose()
   }
+}
 
+const EnhancedContainer = enhanceWithClickOutside(ContainerClickOutside)
+
+class FavoriteLogin extends Component {
   render() {
     return (
-      <Container>
-        <Text></Text>
-      </Container>
+      <Background>
+        <EnhancedContainer onClose={this.props.onClose}>
+          <Row justifyContent="center">
+            <HeartContainer>
+              <FontAwesomeIcon icon={faHeart} />
+            </HeartContainer>
+          </Row>
+          <Text>Salvar imóvel</Text>
+          <Text>Este imóvel será salvo na sua aba de favoritos. Para isso, só precisamos do seu nome e telefone.</Text>
+          <Input
+            label="Nome"
+          />
+        </EnhancedContainer>
+      </Background>
     )
   }
 }
@@ -24,4 +48,4 @@ FavoriteLogin.propTypes = {
   onClose: PropTypes.func.isRequired
 }
 
-export default enhanceWithClickOutside(FavoriteLogin)
+export default FavoriteLogin
--- components/shared/Common/Buttons/Like/FavoriteLogin/styles.js
+++ components/shared/Common/Buttons/Like/FavoriteLogin/styles.js
@@ -1,16 +1,55 @@
 import styled from 'styled-components'
+import theme from '@emcasa/ui'
 import View from '@emcasa/ui-dom/components/View'
 import {breakpoint} from '@emcasa/ui/lib/styles'
 
-export const Container = styled(View)`
+const WIDTH = 480
+const HEIGHT = 640
+
+export const Background = styled(View)`
   position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 5;
+`
+
+export const Container = styled(View)`
+  cursor: default;
   background-color: white;
-  width: 360px;
-  height: 640px;
+  width: ${WIDTH}px;
+  height: ${HEIGHT}px;
+  margin: calc((100vh - ${HEIGHT}px) / 2) auto;
   z-index: 1;
 
   @media ${breakpoint.only('mobile')} {
     width: 100%;
     height: 100%;
   }
+`
+
+export const HeartContainer = styled(View)`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: 2px solid ${theme.colors.dark};
+  width: 42px;
+  height: 42px;
+  border-radius: 50%;
+
+  svg {
+    width: 15px;
+
+    path {
+      fill: ${theme.colors.white};
+      stroke: ${theme.colors.dark};
+      fill-opacity: 0;
+      stroke-width: 60;
+    }
+  }
+
 `
