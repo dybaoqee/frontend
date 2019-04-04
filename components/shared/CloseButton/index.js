ddb4041ca58c1102dea72a4a2d36ce272a6d17ac
Garou <jrgarou@gmail.com>
2019-04-04 15:33:33 -0300

    CloseButton redesign

--- components/shared/CloseButton/index.js
+++ components/shared/CloseButton/index.js
@@ -1,30 +1,9 @@
-import styled from 'styled-components'
-import theme from '@emcasa/ui'
+import Button from '@emcasa/ui-dom/components/Button'
 import Icon from '@emcasa/ui-dom/components/Icon'
 
-const CloseButtonContainer = styled.div`
-  cursor: pointer;
-  display: flex;
-  position: absolute;
-  top: ${theme.space[2]}px;
-  right: ${theme.space[2]}px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 2px;
-  border: none;
-  width: 40px;
-  height: 40px;
-  margin: 0;
-  padding: 0;
-`
-
-const CloseIcon = styled(Icon)`
-  padding-top: 6px;
-`
-
 const CloseButton = (props) =>
-  <CloseButtonContainer onClick={props.onClick}>
-    <CloseIcon name="times" color={props.color ? props.color : theme.colors.dark} size={18} />
-  </CloseButtonContainer>
+  <Button onClick={props.onClick}>
+    <Icon name="times" size={18} />
+  </Button>
 
 export default CloseButton
