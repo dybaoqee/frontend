eb868d362a937cafd8aec4fc39e9e4fbd7d106a4
Garou <jrgarou@gmail.com>
2019-04-05 11:06:57 -0300

    Remove Feed scroll from mobile

--- components/listings/show/RelatedListings/styles.js
+++ components/listings/show/RelatedListings/styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
+import theme from 'config/theme'
 import Row from '@emcasa/ui-dom/components/Row'
 
 export default styled(Row)`
   width: 100%;
+  padding: ${theme.space[6]}px 0 ${theme.space[6]}px;
 `
--- components/shared/Listing/Feed/styles.js
+++ components/shared/Listing/Feed/styles.js
@@ -16,24 +16,25 @@ export const Container = styled.div`
 export const ListingsContainer = styled.div`
   box-sizing: border-box;
   display: flex;
-  flex-wrap: nowrap;
-  align-items: center;
-  justify-content: flex-start;
-  overflow-x: scroll;
+  flex-wrap: wrap;
+
+  @media ${breakpoint.up('desktop')} {
+    flex-wrap: nowrap;
+    overflow-x: scroll;
+  }
 `
 
 export const SubTitle = Text.withComponent('h3')
 
 export const Gradient = styled.div`
   pointer-events: none;
-  position: absolute;
-  top: 0;
-  right: ${theme.space[4]}px;
-  width: 60px;
-  height: 100%;
-  background-image: linear-gradient(to right, transparent, white);
 
-  @media ${breakpoint.down('tablet')} {
+  @media ${breakpoint.up('desktop')} {
+    position: absolute;
+    top: 0;
     right: ${theme.space[4]}px;
+    width: 60px;
+    height: 100%;
+    background-image: linear-gradient(to right, transparent, white);
   }
 `
