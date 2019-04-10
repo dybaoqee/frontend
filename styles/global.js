39b77f2abae635e14be246602542e1c6c3d3bbff
Luis Gustavo Beligante <luis.beligante@emcasa.com>
2019-04-10 09:30:05 -0300

    Adding font-face for Rubik

--- styles/global.js
+++ styles/global.js
@@ -4,6 +4,20 @@ import theme from 'config/theme'
 import * as colors from 'constants/colors'
 
 export default injectGlobal`
+@font-face {
+  font-family: 'Rubik';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Rubik'), local('Rubik-Regular'), url(https://fonts.gstatic.com/s/rubik/v8/iJWKBXyIfDnIV7nBrXyw023e.woff2) format('woff2');
+}
+
+@font-face {
+  font-family: 'Rubik';
+  font-style: normal;
+  font-weight: 500;
+  src: local('Rubik Medium'), local('Rubik-Medium'), url(https://fonts.gstatic.com/s/rubik/v8/iJWHBXyIfDnIV7Eyjmmd8WD07oB-.woff2) format('woff2');
+}
+
 html {
   color: ${colors.text};
   font-size: 100%;
