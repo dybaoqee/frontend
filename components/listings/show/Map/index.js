08b2bb4d928c254f4a32e04c98264ea015c73ee6
Garou <jrgarou@gmail.com>
2019-04-24 11:23:48 -0300

    Update map by streetviewservice and show "no streetview" message

--- components/listings/show/Map/index.js
+++ components/listings/show/Map/index.js
@@ -2,27 +2,38 @@ import {Component} from 'react'
 import ReactDOM from 'react-dom'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faHome from '@fortawesome/fontawesome-free-solid/faHome'
-import Container from './styles'
+import Container, {AlertText} from './styles'
 import GoogleMapReact from 'google-map-react'
+import Text from '@emcasa/ui-dom/components/Text'
 import MapMarker from 'components/shared/Map/Marker'
 
 export default class ListingMap extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      panorama: null
+      maps: null,
+      panorama: null,
+      streetviewService: null,
+      streetviewStatus: true
     }
   }
 
-  processStreetViewService = (data) => {
-    const {panorama} = this.state
+  processStreetViewService = (data, status) => {
+    const {isVisible} = this.props
+    const {panorama, streetviewStatus} = this.state
+    const svStatus = status === 'OK'
 
-    if (panorama) {
+    if (streetviewStatus !== svStatus) {
+      this.setState({streetviewStatus: svStatus})
+    }
+
+    if (panorama && data){
       panorama.setPov({
         heading: 270,
         pitch: 0
       })
       panorama.setPano(data.location.pano)
+      panorama.setVisible(isVisible)
     }
   }
 
@@ -31,7 +42,7 @@ export default class ListingMap extends Component {
     const {lat, lng} = listing.address
 
     if (streetView) {
-      const sv = new google.maps.StreetViewService()
+      const streetviewService = new maps.StreetViewService()
       const panorama = new maps.StreetViewPanorama(
         ReactDOM.findDOMNode(this.refs.panorama), {
           position: {lat: lat, lng: lng},
@@ -41,29 +52,40 @@ export default class ListingMap extends Component {
         }
       )
       map.setStreetView(panorama)
-      sv.getPanorama({location: {lat: lat, lng: lng}, source: maps.StreetViewSource.OUTDOOR}, this.processStreetViewService)
-      this.setState({panorama: panorama})
+      streetviewService.getPanorama({location: {lat: lat, lng: lng}, source: maps.StreetViewSource.OUTDOOR}, this.processStreetViewService)
+      this.setState({
+        maps: maps,
+        streetviewService: streetviewService,
+        panorama: panorama
+      })
     }
   }
 
   componentDidUpdate() {
-    const {listing, isVisible, streetView} = this.props
-    const {panorama} = this.state
+    const {listing, streetView, isVisible} = this.props
+    const {maps, streetviewService, panorama} = this.state
     const {lat, lng} = listing.address
 
-    if (streetView && panorama) {
-      panorama.setPosition({lat: lat, lng: lng})
-      panorama.setVisible(isVisible)
+    if (isVisible) {
+      if (streetView && streetviewService) {
+        streetviewService.getPanorama({location: {lat: lat, lng: lng}, source: maps.StreetViewSource.OUTDOOR}, this.processStreetViewService)
+      }
+    } else {
+      if (panorama) {
+        panorama.setVisible(isVisible)
+      }
     }
   }
 
   render() {
     const {listing, streetView} = this.props
+    const {streetviewStatus} = this.state
     const {lat, lng} = listing.address
     const text = <FontAwesomeIcon icon={faHome} />
 
     return (
       <Container>
+        {!streetviewStatus && <AlertText><Text color="white">Não há visualização da rua para este endereço.</Text></AlertText>}
         <GoogleMapReact
           bootstrapURLKeys={{
             key: process.env.GOOGLE_MAPS_KEY,
