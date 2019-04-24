3ff2824cdd5d1331c9273318e22f2c0ae628fd45
Lucas Tulio <lucasctulio@gmail.com>
2019-04-24 18:04:42 -0300

    AB test to toggle asking for user name when favoriting a listing

--- components/shared/Flagr/tests.js
+++ components/shared/Flagr/tests.js
@@ -1,2 +1,5 @@
 // Show the 3d tour button when gallery is on full screen mode
 export const TEST_MATTERPORT_BUTTON_TEXT = 'test_matterport_button_text'
+
+// Ask for user name when favoriting a listing or not
+export const TEST_SAVE_LISTING_USER_NAME = 'test_save_listing_user_name'
